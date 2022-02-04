df6ee19a0fd38b842014c30b899138ea7e8c7c13
Grzegorz Ilach <grzegorz@ilach.pl>
2022-02-04 16:58:18 +0100

    My Training section (#32)
    * feature: My Training page with mock data
    
    * feature: add My Training pages routes
    
    * feature: add My Upcoming Training page with mock data

---
+++ src/pages/MyTraining/MyResources.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { NavLink } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+
+import { Icon } from '@app/components/Icon'
+import { Typography } from '@app/components/Typography'
+
+type MyResourcesProps = unknown
+
+export const MyResources: React.FC<MyResourcesProps> = () => {
+  const { t } = useTranslation()
+
+  return (
+    <div className="flex">
+      <div className="w-48 p-8 hidden sm:flex sm:flex-col">
+        <div className="flex mb-8">
+          <NavLink to=".." className="flex flex-row">
+            <Icon name="arrow-left" />
+            <Typography className="ml-2">{t('common.back')}</Typography>
+          </NavLink>
+        </div>
+      </div>
+
+      <div className="p-8">
+        <div className="pb-8 font-light text-2xl sm:text-4xl">
+          {t('pages.my-training.resources.title')}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/MyTraining/MyTrainingPage.tsx
+++ src/pages/MyTraining/MyTrainingPage.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 type MyTrainingPageProps = unknown
 
 export const MyTrainingPage: React.FC<MyTrainingPageProps> = () => {
-  return <div className="px-4">My Training Page</div>
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
 }
