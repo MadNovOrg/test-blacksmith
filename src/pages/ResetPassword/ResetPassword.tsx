29cbd457873b47d7bc0a3ba6333b152e4a03affc
Andrew Winterbotham <97905032+AndrewTeamTeach@users.noreply.github.com>
2022-02-16 08:36:05 +0000

    fix issue in build with module not exported (#43)
    * fix issue in build with module not exported
    
    * fix ts error
    
    * wip
    
    * code review suggestions
    
    * remove dependency
    
    * chore: lint
    
    * chore: fix comment

--- src/pages/ResetPassword/ResetPassword.tsx
+++ src/pages/ResetPassword/ResetPassword.tsx
@@ -1,16 +1,25 @@
-import React, { useState } from 'react'
+import React, {
+  useEffect,
+  useState,
+  FocusEvent,
+  KeyboardEvent,
+  ChangeEvent,
+} from 'react'
 import { Auth } from 'aws-amplify'
 import {
   createSearchParams,
   useSearchParams,
   useNavigate,
 } from 'react-router-dom'
-import VerificationInput from 'react-verification-input'
 import clsx from 'clsx'
 import { useTranslation } from 'react-i18next'
 
 import { Input } from '@app/components/Input'
 import { LoggedOutHeader } from '@app/components/LoggedOutHeader'
+
+type VerificationCodeState = {
+  [key: string]: string
+}
 
 type E = {
   code: string
@@ -60,16 +69,97 @@ const errorCodeToMessageMapping: Map = {
   },
 }
 
+const useVerificationInputFields = () => {
+  const numFields = 6
+  const [verificationCodeValues, setVerificationCodeValues] =
+    React.useState<VerificationCodeState>({
+      '0': '',
+      '1': '',
+      '2': '',
+      '3': '',
+      '4': '',
+      '5': '',
+    })
+
+  return {
+    verificationCodeValues,
+    setVerificationCodeValues,
+    handleFocus: (e: FocusEvent<HTMLInputElement>) => {
+      const { name } = e.target
+      const fieldIndex = name.split('-')[1]
+      setVerificationCodeValues(s => ({
+        ...s,
+        [fieldIndex]: '',
+      }))
+    },
+    handleBackSpace: (e: KeyboardEvent<HTMLInputElement>) => {
+      const { maxLength, value, name } = e.target as HTMLInputElement
+      const fieldIndex = name.split('-')[1]
+
+      // Check if they hit the max character length
+      if (value.length <= maxLength) {
+        // Check if first character
+        if (parseInt(fieldIndex, 10) !== 0) {
+          // Get the previous input field
+          const prevSibling = document.querySelector(
+            `input[name=code-${parseInt(fieldIndex, 10) - 1}]`
+          )
+
+          // If found, focus the previous field
+          if (prevSibling !== null) {
+            ;(prevSibling as HTMLElement).focus()
+          }
+        }
+      }
+
+      setVerificationCodeValues(s => ({
+        ...s,
+        [fieldIndex.toString()]: '',
+      }))
+    },
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => {
+      const { maxLength, value, name } = e.target
+      const fieldIndex = name.split('-')[1]
+
+      // Check if they hit the max character length
+      if (value.length >= maxLength) {
+        // Check if it's not the last input field
+        if (parseInt(fieldIndex, 10) < numFields) {
+          // Get the next input field
+          const nextSibling = document.querySelector(
+            `input[name=code-${parseInt(fieldIndex, 10) + 1}]`
+          )
+
+          // If found, focus the next field
+          if (nextSibling !== null) {
+            ;(nextSibling as HTMLElement).focus()
+          }
+        }
+      }
+
+      setVerificationCodeValues(s => ({
+        ...s,
+        [fieldIndex]: value,
+      }))
+    },
+  }
+}
+
 export const ResetPasswordPage = () => {
+  const {
+    handleChange,
+    handleFocus,
+    handleBackSpace,
+    verificationCodeValues,
+    setVerificationCodeValues,
+  } = useVerificationInputFields()
   const { t } = useTranslation()
   const [searchParams] = useSearchParams()
   const navigate = useNavigate()
 
   const email = searchParams.get('email')
+  const code = searchParams.get('confirmation_code')
 
-  const [code, setCode] = useState<string | null>(
-    searchParams.get('confirmation_code')
-  )
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [newPassword, setNewPassword] = useState<string>('')
   const [alreadyResent, setAlreadyResent] = useState(false)
@@ -81,6 +171,17 @@ export const ResetPasswordPage = () => {
     passwordErrorMessage: '',
     codeErrorMessage: '',
   })
+
+  useEffect(() => {
+    if (code !== null) {
+      for (let i = 0; i < (code as string).length; i++) {
+        setVerificationCodeValues(s => ({
+          ...s,
+          [i]: (code as string)[i],
+        }))
+      }
+    }
+  }, [code, setVerificationCodeValues])
 
   const onSubmit = async (event: React.ChangeEvent<HTMLFormElement>) => {
     event.preventDefault()
@@ -103,7 +204,13 @@ export const ResetPasswordPage = () => {
       }))
     }
 
-    if (code === null || code.length != 6) {
+    let codeInputted = ''
+    for (let i = 0; i < 6; i++) {
+      codeInputted += verificationCodeValues[i.toString()]
+    }
+    const codeToSubmit = code || codeInputted
+
+    if (codeToSubmit === null || codeToSubmit.length != 6) {
       error = true
       setErrorState(s => ({
         ...s,
@@ -123,7 +230,7 @@ export const ResetPasswordPage = () => {
     try {
       await Auth.forgotPasswordSubmit(
         email as string,
-        code as string,
+        codeToSubmit,
         newPassword
       )
       navigate({
@@ -176,7 +283,8 @@ export const ResetPasswordPage = () => {
                 onClick={async () => {
                   setAlreadyResent(true)
 
-                  // tell cognito again to send either email or sms with pw reset code and link to reset form
+                  // tell cognito again to send either email or sms with pw
+                  // reset code and link to reset form
                   await Auth.forgotPassword(email as string)
                 }}
               >
@@ -249,28 +357,51 @@ export const ResetPasswordPage = () => {
             </div>
 
             <div className="-ml-4">
-              <div className="text-xs ml-4 mb-4">Enter Passcode</div>
-              <VerificationInput
-                onChange={val => {
-                  setErrorState(s => ({
-                    ...s,
-                    codeErrorMessage: '',
-                  }))
-                  setCode(val)
-                }}
-                placeholder="#"
-                length={6}
-                removeDefaultStyles={true}
-                value={code as string}
-                classNames={{
-                  character: clsx(
-                    {
-                      'border-red': errorState.codeErrorMessage.length > 0,
-                    },
-                    'text-4xl font-thin border-b-2 ml-4 w-12 border-grey4'
-                  ),
-                }}
-              />
+              <div
+                className={clsx(
+                  { 'text-red': errorState.codeErrorMessage.length > 0 },
+                  'text-xs ml-4 mb-4'
+                )}
+              >
+                {t('pages.reset-password.enter-passcode')}
+              </div>
+              {Object.keys(verificationCodeValues).map(key => {
+                return (
+                  <input
+                    onPaste={e => {
+                      e.clipboardData.items[0].getAsString(text => {
+                        for (let i = 0; i < text.length; i++) {
+                          setVerificationCodeValues(s => ({
+                            ...s,
+                            [i]: text[i],
+                          }))
+                        }
+                      })
+                    }}
+                    onKeyDown={e => {
+                      if (e.key === 'Backspace') {
+                        handleBackSpace(e)
+                      }
+                    }}
+                    value={verificationCodeValues[key]}
+                    placeholder={'#'}
+                    key={key}
+                    maxLength={1}
+                    type="text"
+                    name={`code-${key}`}
+                    onChange={handleChange}
+                    onFocus={handleFocus}
+                    className={clsx(
+                      {
+                        'border-red': errorState.codeErrorMessage.length > 0,
+                        'border-gray-300':
+                          errorState.codeErrorMessage.length === 0,
+                      },
+                      'outline-0 border-b-2 border-l-0 border-r-0 border-t-0 focus:ring-0 placeholder-gray-400 placeholder:text-3xl text-3xl font-thin ml-4 w-12'
+                    )}
+                  ></input>
+                )
+              })}
             </div>
 
             <div className="-ml-4">
