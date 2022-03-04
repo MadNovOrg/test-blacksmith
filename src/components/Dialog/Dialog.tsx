7fc56b2f22be034ed3ac2a4ef9a78a90e27a4016
Alex Parra <alexpds@gmail.com>
2022-03-04 17:13:00 +0000

    feat: modal helper component (#114)
    * feat: modal helper component
    
    * feat(Modal): add tests
    
    * fix: rename Modal to Dialog as per comments

---
+++ src/components/Dialog/Dialog.tsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import {
+  IconButton,
+  Dialog as MUIDialog,
+  DialogTitle,
+  DialogContent,
+} from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+
+type Props = {
+  id?: string
+  open: boolean
+  title?: string | React.ReactNode
+  showClose?: boolean
+  onClose: () => void
+}
+
+export const Dialog: React.FC<Props> = ({
+  id,
+  open,
+  title,
+  showClose = true,
+  onClose,
+  children,
+}) => {
+  return (
+    <MUIDialog open={open} maxWidth={false} onClose={onClose}>
+      <DialogTitle
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        {title ?? null}
+        {showClose ? (
+          <IconButton
+            data-testid={id ? `dialog-${id}-close` : 'dialog-close'}
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              ml: 2,
+              mr: -1.5,
+              color: 'grey.500',
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+      <DialogContent>{children}</DialogContent>
+    </MUIDialog>
+  )
+}
