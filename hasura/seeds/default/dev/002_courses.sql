712cdb42020ef51640772d465360a22543213918
Andrew Winterbotham <97905032+AndrewTeamTeach@users.noreply.github.com>
2022-03-11 17:50:49 +0000

    update seeds (#161)

--- hasura/seeds/default/dev/002_courses.sql
+++ hasura/seeds/default/dev/002_courses.sql
@@ -1,15 +1,15 @@
-INSERT INTO public.course (id, description, name, course_type, course_delivery_type, course_level, organization_id, reaccreditation)
-VALUES ('45045398-f757-4ece-85c0-bdf2d95d7cee', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Level One', 'OPEN', 'F2F', 'LEVEL_1', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false), 
-    ('913777a7-07fd-46d6-8ad8-d7498f630aea', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Level Two', 'OPEN', 'F2F', 'LEVEL_2', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false),
-    ('bd42bbfb-9b7d-4028-ba50-6f8bbb6884d6', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Advanced Modules', 'OPEN', 'F2F', 'ADVANCED', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false),
-    ('f1fd057b-547d-470b-aad8-b946a9eb8adc', 'Some description.', 'Positive Behaviour Training: Level One', 'OPEN', 'F2F', 'LEVEL_1', null, false), 
-    ('c6c576cb-e3bf-482a-8b50-df0f5e0afc55', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'OPEN', 'F2F', 'LEVEL_2', null, false),
-    ('32e3324a-d27e-4c05-952d-5bb4e6e61e19', 'Very long description of the course.', 'Positive Behaviour Training: Advanced Modules', 'CLOSED', 'F2F', 'ADVANCED', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false),
-    ('f37ef3b0-d705-4ade-99bb-5879dd218dbd', 'Some description.', 'Positive Behaviour Training: Level One', 'CLOSED', 'BLENDED', 'LEVEL_1', null, false), 
-    ('7ea83d8f-9eaa-46e1-9e2b-49170ddb1666', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'CLOSED', 'BLENDED', 'LEVEL_2', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false),
-    ('eba38da7-aa4d-4aed-a269-2db6d93f09fc', 'Very long description of the course.', 'Positive Behaviour Training: Advanced Modules', 'CLOSED', 'BLENDED', 'ADVANCED', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false),
-    ('6770c5e1-f21b-45e9-a1f4-084b079937ec', 'Some description.', 'Positive Behaviour Training: Level One', 'INDIRECT', 'VIRTUAL', 'LEVEL_1', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false), 
-    ('64abf3d3-b7ab-4d8e-b625-bf921ecf5ebc', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'OPEN', 'VIRTUAL', 'LEVEL_2', null, false);
+INSERT INTO public.course (id, description, name, course_type, course_delivery_type, course_level, organization_id, reaccreditation, trainer_profile_id)
+VALUES ('45045398-f757-4ece-85c0-bdf2d95d7cee', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Level One', 'OPEN', 'F2F', 'LEVEL_1', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'), 
+    ('913777a7-07fd-46d6-8ad8-d7498f630aea', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Level Two', 'OPEN', 'F2F', 'LEVEL_2', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('bd42bbfb-9b7d-4028-ba50-6f8bbb6884d6', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.', 'Positive Behaviour Training: Advanced Modules', 'OPEN', 'F2F', 'ADVANCED', '55320dc6-cfb0-41fb-9000-ca7eb9d2894d', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('f1fd057b-547d-470b-aad8-b946a9eb8adc', 'Some description.', 'Positive Behaviour Training: Level One', 'OPEN', 'F2F', 'LEVEL_1', null, false, '13a223a8-2184-42f1-ba37-b49e115e59a2'), 
+    ('c6c576cb-e3bf-482a-8b50-df0f5e0afc55', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'OPEN', 'F2F', 'LEVEL_2', null, false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('32e3324a-d27e-4c05-952d-5bb4e6e61e19', 'Very long description of the course.', 'Positive Behaviour Training: Advanced Modules', 'CLOSED', 'F2F', 'ADVANCED', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('f37ef3b0-d705-4ade-99bb-5879dd218dbd', 'Some description.', 'Positive Behaviour Training: Level One', 'CLOSED', 'BLENDED', 'LEVEL_1', null, false, '13a223a8-2184-42f1-ba37-b49e115e59a2'), 
+    ('7ea83d8f-9eaa-46e1-9e2b-49170ddb1666', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'CLOSED', 'BLENDED', 'LEVEL_2', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('eba38da7-aa4d-4aed-a269-2db6d93f09fc', 'Very long description of the course.', 'Positive Behaviour Training: Advanced Modules', 'CLOSED', 'BLENDED', 'ADVANCED', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'),
+    ('6770c5e1-f21b-45e9-a1f4-084b079937ec', 'Some description.', 'Positive Behaviour Training: Level One', 'INDIRECT', 'VIRTUAL', 'LEVEL_1', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', false, '13a223a8-2184-42f1-ba37-b49e115e59a2'), 
+    ('64abf3d3-b7ab-4d8e-b625-bf921ecf5ebc', 'Very long description of the course.', 'Positive Behaviour Training: Level Two', 'OPEN', 'VIRTUAL', 'LEVEL_2', null, false, '13a223a8-2184-42f1-ba37-b49e115e59a2');
 
 INSERT INTO public.course_leader (profile_id, course_id, type)
 SELECT profile.id, '45045398-f757-4ece-85c0-bdf2d95d7cee', 'leader'
@@ -37,9 +37,9 @@ INSERT INTO public.course_leader (profile_id, course_id, type) VALUES
 ('13a223a8-2184-42f1-ba37-b49e115e59a2', '6770c5e1-f21b-45e9-a1f4-084b079937ec', 'leader'),
 ('13a223a8-2184-42f1-ba37-b49e115e59a2', '64abf3d3-b7ab-4d8e-b625-bf921ecf5ebc', 'leader');
 
-INSERT INTO public.venue (id, name, address) VALUES
-('bd4e4af5-8822-485c-bf48-16fe0d50729b', 'Birchwood Academy', '{ "city": "Kent" }'::jsonb),
-('2fa3a402-3aa0-4d7a-bbf9-e3dda59cd18b', 'Queen Elizabeth II Centre', '{ "city": "London" }'::jsonb);
+INSERT INTO public.venue (id, name, address, "geoCoordinates") VALUES
+('bd4e4af5-8822-485c-bf48-16fe0d50729b', 'Birchwood Academy', '{ "city": "New York", "country": "America", "addressLineOne": "10 Whitehart Lane", "addressLineTwo": "Kings Street" }'::jsonb, '(40.730610, -73.935242)'::point),
+('2fa3a402-3aa0-4d7a-bbf9-e3dda59cd18b', 'Queen Elizabeth II Centre', '{ "city": "New York", "country": "America", "addressLineOne": "10 Whitehart Lane", "addressLineTwo": "Kings Street" }'::jsonb, '(40.730610, -73.935242)'::point);
 
 INSERT INTO public.course_schedule (name, type, start, "end", course_id, venue_id) VALUES
 ('name', 'PHYSICAL', '2022-04-05T09:00:00+00:00', '2022-04-05T17:00:00+00:00', '45045398-f757-4ece-85c0-bdf2d95d7cee', 'bd4e4af5-8822-485c-bf48-16fe0d50729b'),
