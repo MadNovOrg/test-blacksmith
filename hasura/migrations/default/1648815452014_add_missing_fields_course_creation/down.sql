469d97b2197c81329b2f44ef1a689206f4de3131
Grzegorz Ilach <grzegorz@ilach.pl>
2022-04-01 17:00:08 +0200

    chore: add missing fields for course creation (#269)

---
+++ hasura/migrations/default/1648815452014_add_missing_fields_course_creation/down.sql
@@ -0,0 +1,5 @@
+alter table "public"."course" drop column "go1integration";
+
+alter table "public"."course" drop column "aol_cost_of_course";
+
+alter table "public"."course_schedule" drop column "virtual_link";
