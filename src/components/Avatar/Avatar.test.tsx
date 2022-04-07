f9b20f8f180bbc493d784aa09c9933974e96181d
Alex Parra <alexpds@gmail.com>
2022-04-07 12:14:09 +0100

    fix: Avatar to work as avatar of Chip (#304)

---
+++ src/components/Avatar/Avatar.test.tsx
@@ -0,0 +1,67 @@
+import React from 'react'
+
+import { render, screen, within, chance } from '@test/index'
+
+import { Avatar } from './Avatar'
+
+describe('component: Avatar', () => {
+  it('renders as expected without props', async () => {
+    const { container } = render(<Avatar />)
+
+    const avatar = container.querySelector('.MuiAvatar-root')
+    expect(avatar).toBeInTheDocument()
+    expect(
+      within(avatar as HTMLElement).getByTestId('PersonIcon')
+    ).toBeInTheDocument()
+  })
+
+  it('renders image from src provided', async () => {
+    const img = chance.url()
+
+    const { container } = render(<Avatar src={img} />)
+
+    const avatarImg = container.querySelector(`[src="${img}"]`)
+    expect(avatarImg).toBeInTheDocument()
+  })
+
+  it('renders image from src provided even if name is provided', async () => {
+    const img = chance.url()
+    const [firstName, lastName] = [chance.first(), chance.last()]
+    const initials = `${firstName[0]}${lastName[0]}`
+
+    const { container } = render(<Avatar src={img} />)
+
+    const avatarImg = container.querySelector(`[src="${img}"]`)
+    expect(avatarImg).toBeInTheDocument()
+    expect(screen.queryByText(initials)).toBeNull()
+  })
+
+  describe('renders initials when src is missing', () => {
+    it('supports names with first and last', async () => {
+      const [firstName, lastName] = [chance.first(), chance.last()]
+      const initials = `${firstName[0]}${lastName[0]}`
+
+      render(<Avatar name={`${firstName} ${lastName}`} />)
+
+      expect(screen.queryByText(initials)).toBeInTheDocument()
+    })
+
+    it('supports names with first, middle and last', async () => {
+      const [firstName, lastName] = [chance.first(), chance.last()]
+      const initials = `${firstName[0]}${lastName[0]}`
+
+      render(<Avatar name={`${firstName} ??? ${lastName}`} />)
+
+      expect(screen.queryByText(initials)).toBeInTheDocument()
+    })
+
+    it('supports single names', async () => {
+      const [firstName] = [chance.first()]
+      const initials = firstName[0]
+
+      render(<Avatar name={firstName} />)
+
+      expect(screen.queryByText(initials)).toBeInTheDocument()
+    })
+  })
+})
--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,6 +1,42 @@
 import { Avatar as MuiAvatar, SxProps } from '@mui/material'
 import React from 'react'
 
+type AvatarProps = {
+  name?: string
+  src?: string
+  size?: number
+  sx?: SxProps
+  className?: string
+}
+
+export const Avatar: React.FC<AvatarProps> = ({
+  name,
+  src,
+  size,
+  sx,
+  className,
+}) => {
+  const props = {
+    src,
+    className,
+    sx: {
+      ...(name && { bgcolor: stringToColor(name) }),
+      ...(size && { width: size, height: size, fontSize: size / 2 }),
+      ...sx,
+    },
+  }
+
+  const names = (name ?? '').split(' ')
+  const [firstLetter = ''] = names[0]
+  const [secondLetter = ''] = names.length > 1 ? names.slice(-1)[0] : []
+
+  return (
+    <MuiAvatar role="img" {...props}>
+      {name ? firstLetter + secondLetter : null}
+    </MuiAvatar>
+  )
+}
+
 function stringToColor(string: string) {
   let hash = 0
   let i
@@ -19,35 +55,4 @@ function stringToColor(string: string) {
   /* eslint-enable no-bitwise */
 
   return color
-}
-
-function stringAvatar(name = '', size = 40, sx?: SxProps) {
-  if (!name) return {}
-
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-      width: size,
-      height: size,
-      fontSize: size / 2,
-      ...sx,
-    },
-    children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
-  }
-}
-
-type AvatarProps = {
-  name?: string
-  src?: string
-  size?: number
-  sx?: SxProps
-}
-
-export const Avatar: React.FC<AvatarProps> = ({ src, name, size, sx }) => {
-  return (
-    <MuiAvatar
-      alt="avatar"
-      {...(src ? { src } : stringAvatar(name, size, sx))}
-    />
-  )
 }
