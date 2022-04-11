10a42d15bb89dc0da568aba0de9a63b7ed7f0f77
Grzegorz Ilach <grzegorz@ilach.pl>
2022-04-11 16:43:12 +0200

    feat: course certifications page (#316)

--- hasura/migrations/default/1649412213060_course_expiry_date/up.sql
+++ hasura/migrations/default/1649412213060_course_expiry_date/up.sql
@@ -8,7 +8,7 @@ from "public"."course_participant" participant
 join "public"."course" course on participant.course_id = course.id
 where course.course_level = 'LEVEL_1';
 
-update "public"."course_certificate" set expiry_date = created_at + interval '24 months'
+update "public"."course_certificate" certificate set expiry_date = certificate.created_at + interval '24 months'
 from "public"."course_participant" participant
 join "public"."course" course on participant.course_id = course.id
 where course.course_level = 'LEVEL_2';
