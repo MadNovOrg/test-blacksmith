30fec8ba2d241ccbfe56b15260eda082939adf06
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-05-04 11:07:33 +0200

    fix(TTHP-378): trainer user accept invite (#389)

---
+++ src/components/ChangeRole/index.tsx
@@ -0,0 +1,18 @@
+import React, { useEffect } from 'react'
+
+import { useAuth } from '@app/context/auth'
+import { RoleName } from '@app/types'
+
+type Props = {
+  role: RoleName
+}
+
+export const ChangeRole: React.FC<Props> = ({ role }) => {
+  const { changeRole } = useAuth()
+
+  useEffect(() => {
+    changeRole(role)
+  }, [changeRole, role])
+
+  return null
+}
