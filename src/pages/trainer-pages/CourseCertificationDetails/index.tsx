2eb797a22e762ae4482fbd1d9975a5a6698b1605
Grzegorz Ilach <grzegorz@ilach.pl>
2022-05-23 11:19:07 +0200

    fix: back button for certificate details page (#455)
    * fix: back button for certificate details page
    
    * fix: certificate back button

--- src/pages/trainer-pages/CourseCertificationDetails/index.tsx
+++ src/pages/trainer-pages/CourseCertificationDetails/index.tsx
@@ -5,21 +5,16 @@ import { useParams } from 'react-router-dom'
 
 import { BackButton } from '@app/components/BackButton'
 import { CourseCertification } from '@app/components/CourseCertification'
-import { CourseDetailsTabs } from '@app/pages/trainer-pages/CourseDetails'
 
 export const CourseCertificationDetails = () => {
   const params = useParams()
   const { t } = useTranslation()
-  const courseId = params.id as string
   const certificateId = params.certificateId as string
 
   return (
     <Container>
       <Box mt={1}>
-        <BackButton
-          to={`/courses/${courseId}/details?tab=${CourseDetailsTabs.CERTIFICATIONS}`}
-          label={t('pages.course-grading-details.back-button-text')}
-        />
+        <BackButton label={t('common.back')} />
       </Box>
       <Box mt={4}>
         <CourseCertification certificateId={certificateId} />
