d097c94c2dda4f38c6c27ec75c6dbed809c97111
Salman <salmanmitha@gmail.com>
2022-05-31 11:31:48 +0530

    chore: fix gql response for create user

--- src/queries/invites/create-user.ts
+++ src/queries/invites/create-user.ts
@@ -3,13 +3,19 @@ import { gql } from 'graphql-request'
 export type ParamsType = unknown
 
 export type ResponseType = {
-  createUser: { id: string; email: string; authChallenge: string | null }
+  createUser: {
+    cognitoId: string
+    profileId: string
+    email: string
+    authChallenge: string | null
+  }
 }
 
 export const MUTATION = gql`
   mutation CreateUser {
     createUser {
-      id
+      cognitoId
+      profileId
       email
       authChallenge
     }
