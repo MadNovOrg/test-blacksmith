b451b2a764fa6c89b8a20fa846e5b330368743e6
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-06-14 11:36:41 +0200

    browse by media component (#521)

--- src/pages/MembershipArea/MemberShipArea.tsx
+++ src/pages/MembershipArea/MemberShipArea.tsx
@@ -1,17 +1,29 @@
 import { Box } from '@mui/material'
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import { MembershipHeaderGradient } from '@app/assets'
 import { Footer } from '@app/components/Footer'
 
+import { BrowseByMedia } from './components/BrowseByMedia'
+
 export function MembershipAreaPage() {
+  const location = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [location])
+
   return (
     <Box position="relative">
       <Box sx={{ position: 'absolute', top: 0, right: 0, zIndex: -1 }}>
         <MembershipHeaderGradient width="100%" height="30vh" />
       </Box>
       <Outlet />
+      <Box mb={5}>
+        <BrowseByMedia />
+      </Box>
+
       <Footer />
     </Box>
   )
---
+++ src/pages/MembershipArea/components/BrowseByMedia/index.tsx
@@ -0,0 +1,82 @@
+import { Box, Link, Typography } from '@mui/material'
+import React from 'react'
+import { useTranslation } from 'react-i18next'
+
+import {
+  BlogIcon,
+  EbookIcon,
+  PodcastsIcon,
+  ResearchSummaryIcon,
+  VideoSeriesIcon,
+  WebinarIcon,
+} from '@app/assets'
+import theme from '@app/theme'
+
+const MediaType: React.FC<{
+  icon: React.ElementType
+  label: string
+  linkTo: string
+}> = ({ icon, label, linkTo }) => {
+  const Icon = icon
+
+  return (
+    <Typography mr={4} mb={3} sx={{ verticalAlign: 'middle' }}>
+      <Link href={linkTo}>
+        <Icon
+          style={{ marginRight: theme.spacing(2), verticalAlign: 'middle' }}
+        />
+        {label}
+      </Link>
+    </Typography>
+  )
+}
+
+export const BrowseByMedia: React.FC = () => {
+  const { t } = useTranslation()
+  return (
+    <Box>
+      <Typography variant="h1" color="primary" textAlign="center" mb={3}>
+        {t('pages.membership.components.browse-by-media.title')}
+      </Typography>
+
+      <Box display="flex" justifyContent="center" flexWrap="wrap">
+        <MediaType
+          label={t('pages.membership.components.browse-by-media.podcasts')}
+          linkTo="/membership/podcasts"
+          icon={PodcastsIcon}
+        />
+
+        <MediaType
+          label={t('pages.membership.components.browse-by-media.blogs')}
+          linkTo="/membership/blog"
+          icon={BlogIcon}
+        />
+
+        <MediaType
+          label={t('pages.membership.components.browse-by-media.video-series')}
+          linkTo="/membership/video-series"
+          icon={VideoSeriesIcon}
+        />
+
+        <MediaType
+          label={t('pages.membership.components.browse-by-media.webinars')}
+          linkTo="/membership/webinars"
+          icon={WebinarIcon}
+        />
+
+        <MediaType
+          label={t('pages.membership.components.browse-by-media.ebooks')}
+          linkTo="/membership/ebooks"
+          icon={EbookIcon}
+        />
+        <MediaType
+          label={t(
+            'pages.membership.components.browse-by-media.research-summaries'
+          )}
+          linkTo="/membership/research-summaries"
+          icon={ResearchSummaryIcon}
+        />
+      </Box>
+    </Box>
+  )
+}
