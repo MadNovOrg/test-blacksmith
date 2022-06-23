f0e53a6838129758ce294911dae8dbeed81f1eda
Grzegorz Ilach <grzegorz@ilach.pl>
2022-06-23 13:54:52 +0200

    fix: failing hasura migrations (#547)

--- hasura/migrations/default/1655286939708_org_field_changes/down.sql
+++ hasura/migrations/default/1655286939708_org_field_changes/down.sql
@@ -1,8 +1,8 @@
 alter table "public"."profile" drop column "last_activity";
 alter table "public"."organization" drop constraint "organization_trust_type_fkey";
 drop table "public"."trust_type";
-alter table "public"."organization" alter column "last_activity" drop not null;
 alter table "public"."organization" add column "last_activity" timestamptz;
+alter table "public"."organization" alter column "last_activity" drop not null;
 alter table "public"."profile" drop column "trust_name";
 alter table "public"."profile" drop column "trust_type";
 
