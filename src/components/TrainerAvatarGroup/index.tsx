a637303d2401d74f2a2a107887892955e5a6a380
Toni Leigh Sharpe <toni.sharpe@nearform.com>
2022-09-05 08:12:26 +0200

    fix(TTHP-695): Fix TrainerAvatarGroup component (#638)
    * fix(TTHP-695): Fix TrainerAvatarGroup component
    
    It should not output as a group when there's only one
    
    Fixes https://behaviourhub.atlassian.net/browse/TTHP-695
    
    * fix(TTHP-695): Include tool tip for single user avatars
    
    Fixes https://behaviourhub.atlassian.net/browse/TTHP-695

--- src/components/TrainerAvatarGroup/index.tsx
+++ src/components/TrainerAvatarGroup/index.tsx
@@ -52,38 +52,42 @@ function sortTrainers(a: TrainerAvatar, b: TrainerAvatar): 1 | -1 | 0 {
 export const TrainerAvatarGroup: React.FC<Props> = ({ trainers }) => {
   const { t } = useTranslation()
 
+  function trainerAvatar(trainer: TrainerAvatar, index: number) {
+    return (
+      <LightTooltip
+        key={trainer.id}
+        title={`${t(
+          `components.trainer-avatar-group.${trainerTypeLabelMap[trainer.type]}`
+        )}: ${trainer.profile.fullName}`}
+        placement="top"
+      >
+        <Avatar
+          name={trainer.profile.fullName ?? ''}
+          size={32}
+          sx={{
+            opacity:
+              trainer.status !== Course_Invite_Status_Enum.Accepted
+                ? '0.5'
+                : '1',
+          }}
+          data-testid={`trainer-avatar-${trainer.id}`}
+          data-index={index}
+        />
+      </LightTooltip>
+    )
+  }
+
   if (!trainers?.length) {
     return null
   }
 
   const sortedTrainers = trainers.slice().sort(sortTrainers)
 
-  return (
+  return sortedTrainers.length === 1 ? (
+    trainerAvatar(sortedTrainers[0], 1)
+  ) : (
     <AvatarGroup sx={{ justifyContent: 'center' }}>
-      {sortedTrainers.map((trainer, index) => (
-        <LightTooltip
-          key={trainer.id}
-          title={`${t(
-            `components.trainer-avatar-group.${
-              trainerTypeLabelMap[trainer.type]
-            }`
-          )}: ${trainer.profile.fullName}`}
-          placement="top"
-        >
-          <Avatar
-            name={trainer.profile.fullName ?? ''}
-            size={32}
-            sx={{
-              opacity:
-                trainer.status !== Course_Invite_Status_Enum.Accepted
-                  ? '0.5'
-                  : '1',
-            }}
-            data-testid={`trainer-avatar-${trainer.id}`}
-            data-index={index}
-          />
-        </LightTooltip>
-      ))}
+      {sortedTrainers.map(trainerAvatar)}
     </AvatarGroup>
   )
 }
