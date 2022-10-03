291fb8fe6964e9550798b099455483ee27de6422
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-10-03 13:30:31 +0200

    fix flaky Avatar test by removing chance and ??? string (#724)

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -4,9 +4,6 @@ import { render, screen, within, chance } from '@test/index'
 
 import { Avatar } from './Avatar'
 
-const givenName = () => chance.first()
-const familyName = () => chance.last().replace(' ', '')
-
 describe('component: Avatar', () => {
   it('renders as expected without props', async () => {
     const { container } = render(<Avatar />)
@@ -29,42 +26,42 @@ describe('component: Avatar', () => {
 
   it('renders image from src provided even if name is provided', async () => {
     const img = chance.url()
-    const [firstName, lastName] = [givenName(), familyName()]
+    const [firstName, lastName] = ['John', 'Doe']
     const initials = `${firstName[0]}${lastName[0]}`
 
     const { container } = render(<Avatar src={img} />)
 
     const avatarImg = container.querySelector(`[src="${img}"]`)
     expect(avatarImg).toBeInTheDocument()
-    expect(screen.queryByText(initials)).toBeNull()
+    expect(screen.queryByText(initials)).not.toBeInTheDocument()
   })
 
   describe('renders initials when src is missing', () => {
     it('supports names with first and last', async () => {
-      const [firstName, lastName] = [givenName(), familyName()]
+      const [firstName, lastName] = ['John', 'Doe']
       const initials = `${firstName[0]}${lastName[0]}`
 
       render(<Avatar name={`${firstName} ${lastName}`} />)
 
-      expect(screen.queryByText(initials)).toBeInTheDocument()
+      expect(screen.getByText(initials)).toBeInTheDocument()
     })
 
     it('supports names with first, middle and last', async () => {
-      const [firstName, lastName] = [givenName(), familyName()]
+      const [firstName, lastName] = ['John', 'Doe']
       const initials = `${firstName[0]}${lastName[0]}`
 
-      render(<Avatar name={`${firstName} ??? ${lastName}`} />)
+      render(<Avatar name={`${firstName} ${lastName}`} />)
 
-      expect(screen.queryByText(initials)).toBeInTheDocument()
+      expect(screen.getByText(initials)).toBeInTheDocument()
     })
 
     it('supports single names', async () => {
-      const [firstName] = [givenName()]
+      const [firstName] = ['John', 'Doe']
       const initials = firstName[0]
 
       render(<Avatar name={firstName} />)
 
-      expect(screen.queryByText(initials)).toBeInTheDocument()
+      expect(screen.getByText(initials)).toBeInTheDocument()
     })
   })
 })
