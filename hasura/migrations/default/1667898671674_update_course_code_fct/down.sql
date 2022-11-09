ab344040253c426dc3766680bd1df7b9eb48f8c3
Grzegorz Ilach <grzegorz.ilach@nearform.com>
2022-11-09 14:33:34 +0100

    fix: course code generation for blended learning (#804)

---
+++ hasura/migrations/default/1667898671674_update_course_code_fct/down.sql
@@ -0,0 +1,11 @@
+CREATE OR REPLACE FUNCTION public.course_code(course_row course)
+    RETURNS text
+    LANGUAGE sql
+    STABLE
+AS $function$
+SELECT course_type_prefix.prefix || '-' || course_level_prefix.prefix || '-' || course_row.id
+FROM course
+         JOIN course_type_prefix ON course.course_type = course_type_prefix.name
+         JOIN course_level_prefix ON course.course_level = course_level_prefix.name
+WHERE course_type_prefix.name = course_row.course_type AND course_level_prefix.name = course_row.course_level;
+$function$;
