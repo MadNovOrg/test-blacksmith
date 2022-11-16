3b9a530533134292211899d4c4d3efe9ced92f63
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-11-16 12:14:06 +0100

    fix syntax error in the down migration (#824)

--- hasura/migrations/default/1668092690107_alter_table_public_trainer_role_type/down.sql
+++ hasura/migrations/default/1668092690107_alter_table_public_trainer_role_type/down.sql
@@ -1,4 +1,4 @@
 
-INSERT INTO trainer_role_type(name) VALUES ('eta')
+INSERT INTO trainer_role_type(name) VALUES ('eta');
 
 DELETE FROM "public"."trainer_role_type" WHERE "name" IN ('moderator', 'trainer-eta', 'aol-eta');
