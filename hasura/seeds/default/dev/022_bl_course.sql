a58de01cbfd88e25a3b294bdc3d2db0c2a63942e
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2022-12-08 11:09:12 +0100

    chore: seed orders (#859)

---
+++ hasura/seeds/default/dev/022_bl_course.sql
@@ -0,0 +1,13 @@
+INSERT INTO public.course (id, name, course_type, course_delivery_type, organization_id, course_level, reaccreditation, go1_integration, course_status, grading_confirmed, min_participants, max_participants) VALUES
+(10030, 'Positive Behaviour Training: Level One', 'INDIRECT', 'F2F', 'c43b2ba0-8630-43e5-9558-f59ee9a224f0', 'LEVEL_1', false, true, 'CONFIRM_MODULES', false, 5, 20);
+
+INSERT INTO public.course_schedule (start, "end", course_id, venue_id) VALUES
+(date(now()) + time '09:00' + interval '2 month', date(now()) + time '17:00' + interval '2 month', 10030, 'bd4e4af5-8822-485c-bf48-16fe0d50729b');
+
+INSERT INTO public.course_trainer (profile_id, course_id, type, status) VALUES
+('13a223a8-2184-42f1-ba37-b49e115e59a2', 10030, 'LEADER', 'ACCEPTED');
+
+INSERT INTO public.order (course_id, profile_id, quantity, payment_method, billing_address, billing_given_name, billing_family_name, billing_email, billing_phone, registrants, organization_id) VALUES
+(10030, '13a223a8-2184-42f1-ba37-b49e115e59a2', 0, 'INVOICE', 'Minerva Centre Thornthwaite close', 'Doe', 'John', 'seed.bl.booking@teamteach.testinator.com', '+441111111', '[]'::jsonb, 'c43b2ba0-8630-43e5-9558-f59ee9a224f0');
+
+SELECT setval('course_id_seq', 10030);
---
+++ scripts/seed-orders.ts
@@ -0,0 +1,83 @@
+import { Chance } from 'chance'
+import { gql, GraphQLClient } from 'graphql-request'
+
+const hasuraSecret = process.env.SECRET ?? 'tth-hasura-key'
+const hasuraEndpoint =
+  process.env.ENDPOINT ?? 'http://localhost:8080/v1/graphql'
+
+if (!hasuraSecret || !hasuraEndpoint) {
+  console.log('Hasura secret and endpoint is needed for seeding orders')
+  process.exit(1)
+}
+
+const chance = new Chance()
+
+const hasuraClient = new GraphQLClient(hasuraEndpoint, {
+  headers: {
+    'x-hasura-admin-secret': hasuraSecret,
+    'x-hasura-user-id': '22015a3e-8907-4333-8811-85f782265a63', // admin@teamteach.testinator.com
+  },
+})
+
+const orders = [
+  {
+    courseId: 10012,
+    organizationId: '55320dc6-cfb0-41fb-9000-ca7eb9d2894d',
+    billingAddress: chance.address(),
+    billingEmail: 'seed.billing1@teamteach.testinator.com',
+    billingFamilyName: 'Doe',
+    billingGivenName: 'John',
+    billingPhone: '+44 1111 11111',
+    clientPurchaseOrder: '',
+    paymentMethod: 'INVOICE',
+    promoCodes: [],
+    quantity: 2,
+    registrants: [
+      'seed.registrant1@teamteach.testinator.com',
+      'seed.registrant2@teamteach.testinator.com',
+    ],
+  },
+  {
+    courseId: 10012,
+    organizationId: '55320dc6-cfb0-41fb-9000-ca7eb9d2894d',
+    billingAddress: chance.address(),
+    billingEmail: 'seed.billing2@teamteach.testinator.com',
+    billingFamilyName: 'Doe',
+    billingGivenName: 'Jane',
+    billingPhone: '+44 1111 11111',
+    clientPurchaseOrder: '',
+    paymentMethod: 'INVOICE',
+    promoCodes: ['CODE_5%_ALL'],
+    quantity: 2,
+    registrants: [
+      'seed.registrant3@teamteach.testinator.com',
+      'seed.registrant4@teamteach.testinator.com',
+    ],
+  },
+]
+
+async function seed() {
+  const MUTATION = gql`
+    mutation SeedOrder($input: CreateOrderInput!) {
+      createOrder(input: $input) {
+        id
+      }
+    }
+  `
+  try {
+    await Promise.all(
+      orders.map(order => hasuraClient.request(MUTATION, { input: order }))
+    )
+  } catch (err) {
+    // Hasura is returning an error but the action is successfully called
+    // The createOrder action is expecting user_id for creating an order
+    // but the hasura is expecting a valid token, but calls the action
+    if (!err.message?.includes('The security token included')) {
+      throw err
+    }
+  }
+
+  console.log('Orders seeded successfully!')
+}
+
+seed()
