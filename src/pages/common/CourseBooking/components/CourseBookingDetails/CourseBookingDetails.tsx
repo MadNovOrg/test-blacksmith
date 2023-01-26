fa7621fb9b5c46dc1fa6578f49cc0550a1920070
Alessandro Santarini <alessandro.santarin@gmail.com>
2023-01-26 09:39:31 +0100

    feat(TTHP-870): merged information into one bar (#1008)
    * feat(TTHP-870): merged information into one bar
    
    * feat(TTHP-870: adjusting screenshots

--- src/pages/common/CourseBooking/components/CourseBookingDetails/CourseBookingDetails.tsx
+++ src/pages/common/CourseBooking/components/CourseBookingDetails/CourseBookingDetails.tsx
@@ -560,10 +560,7 @@ export const CourseBookingDetails: React.FC = () => {
           />
 
           <Alert variant="filled" color="info" severity="info" sx={{ mt: 2 }}>
-            <b>{t('important')}:</b> {t('pages.book-course.notice')}
-          </Alert>
-
-          <Alert variant="filled" color="info" severity="info" sx={{ mt: 2 }}>
+            <b>{t('important')}:</b> {`${t('pages.book-course.notice')}\n`}
             <b>{t('important')}:</b> {t('pages.book-course.repeat-own-email')}
           </Alert>
 
