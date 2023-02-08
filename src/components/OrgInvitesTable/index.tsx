85faae6a0dcbf6ef2fc56e1bbc467577e457c93e
Andrea Forni <1233582+andreaforni@users.noreply.github.com>
2023-02-08 12:22:40 +0100

    fix[TTHP-1027]: Table design inconsistency when viewing pending individuals in an organisation (#1085)
    * fix: change color background of the pagination controls in the pending
    table
    
    * test: update snapshot

--- src/components/OrgInvitesTable/index.tsx
+++ src/components/OrgInvitesTable/index.tsx
@@ -60,112 +60,114 @@ export const OrgInvitesTable: React.FC<OrgInvitesTableParams> = ({ orgId }) => {
   )
 
   return (
-    <TableContainer component={Paper} elevation={0}>
-      <Table>
-        <MuiTableHead>
-          <TableRow>
-            <TableCell>{t('common.email')}</TableCell>
-            <TableCell>
-              {t('pages.org-details.tabs.users.invited-on')}
-            </TableCell>
-            <TableCell>
-              {t('pages.org-details.tabs.users.permissions')}
-            </TableCell>
-            <TableCell>{t('common.status')}</TableCell>
-            <TableCell></TableCell>
-          </TableRow>
-        </MuiTableHead>
-        <TableBody>
-          {loading ? (
-            <TableRow
-              sx={{
-                '&&.MuiTableRow-root': {
-                  backgroundColor: 'grey.300',
-                },
-                '&& .MuiTableCell-root': {
-                  px: 2,
-                  py: 1,
-                  color: 'grey.700',
-                  fontWeight: '600',
-                },
-              }}
-            >
-              <TableCell colSpan={4}>
-                <Stack direction="row" alignItems="center">
-                  <CircularProgress size={20} />
-                </Stack>
+    <>
+      <TableContainer component={Paper} elevation={0}>
+        <Table>
+          <MuiTableHead>
+            <TableRow>
+              <TableCell>{t('common.email')}</TableCell>
+              <TableCell>
+                {t('pages.org-details.tabs.users.invited-on')}
               </TableCell>
+              <TableCell>
+                {t('pages.org-details.tabs.users.permissions')}
+              </TableCell>
+              <TableCell>{t('common.status')}</TableCell>
+              <TableCell></TableCell>
             </TableRow>
-          ) : null}
-
-          <TableNoRows
-            noRecords={!loading && !invites?.length}
-            itemsName={t('invites').toLowerCase()}
-            colSpan={4}
-          />
-
-          {invitesByStatus.map(invite => {
-            return (
-              <TableRow key={invite.id}>
-                <TableCell>{invite.email}</TableCell>
-                <TableCell>
-                  {t('dates.default', { date: invite.createdAt })}
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    label={
-                      invite.isAdmin
-                        ? t('pages.org-details.tabs.users.organization-admin')
-                        : t('pages.org-details.tabs.users.no-permissions')
-                    }
-                    color={invite.isAdmin ? 'success' : 'gray'}
-                    size="small"
-                  />
-                </TableCell>
-                <TableCell>
-                  <Chip
-                    label={t(
-                      `pages.org-details.tabs.users.invite-status.${invite.status.toLowerCase()}`
-                    )}
-                    color={
-                      invite.status === InviteStatus.ACCEPTED
-                        ? 'success'
-                        : invite.status === InviteStatus.DECLINED
-                        ? 'error'
-                        : 'secondary'
-                    }
-                    size="small"
-                  />
-                </TableCell>
-                <TableCell>
-                  {invite.status === InviteStatus.PENDING ? (
-                    <Box display="flex" justifyContent="flex-end">
-                      <Button
-                        data-testid="resend-invite-button"
-                        size="large"
-                        variant="text"
-                        color="primary"
-                        onClick={() => resend(invite)}
-                      >
-                        {t('common.resend')}
-                      </Button>
-                      <Button
-                        data-testid="cancel-invite-button"
-                        size="large"
-                        variant="text"
-                        color="primary"
-                        onClick={() => cancel(invite)}
-                      >
-                        {t('common.cancel-invite')}
-                      </Button>
-                    </Box>
-                  ) : null}
+          </MuiTableHead>
+          <TableBody>
+            {loading ? (
+              <TableRow
+                sx={{
+                  '&&.MuiTableRow-root': {
+                    backgroundColor: 'grey.300',
+                  },
+                  '&& .MuiTableCell-root': {
+                    px: 2,
+                    py: 1,
+                    color: 'grey.700',
+                    fontWeight: '600',
+                  },
+                }}
+              >
+                <TableCell colSpan={4}>
+                  <Stack direction="row" alignItems="center">
+                    <CircularProgress size={20} />
+                  </Stack>
                 </TableCell>
               </TableRow>
-            )
-          })}
-        </TableBody>
-      </Table>
+            ) : null}
+
+            <TableNoRows
+              noRecords={!loading && !invites?.length}
+              itemsName={t('invites').toLowerCase()}
+              colSpan={4}
+            />
+
+            {invitesByStatus.map(invite => {
+              return (
+                <TableRow key={invite.id}>
+                  <TableCell>{invite.email}</TableCell>
+                  <TableCell>
+                    {t('dates.default', { date: invite.createdAt })}
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      label={
+                        invite.isAdmin
+                          ? t('pages.org-details.tabs.users.organization-admin')
+                          : t('pages.org-details.tabs.users.no-permissions')
+                      }
+                      color={invite.isAdmin ? 'success' : 'gray'}
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    <Chip
+                      label={t(
+                        `pages.org-details.tabs.users.invite-status.${invite.status.toLowerCase()}`
+                      )}
+                      color={
+                        invite.status === InviteStatus.ACCEPTED
+                          ? 'success'
+                          : invite.status === InviteStatus.DECLINED
+                          ? 'error'
+                          : 'secondary'
+                      }
+                      size="small"
+                    />
+                  </TableCell>
+                  <TableCell>
+                    {invite.status === InviteStatus.PENDING ? (
+                      <Box display="flex" justifyContent="flex-end">
+                        <Button
+                          data-testid="resend-invite-button"
+                          size="large"
+                          variant="text"
+                          color="primary"
+                          onClick={() => resend(invite)}
+                        >
+                          {t('common.resend')}
+                        </Button>
+                        <Button
+                          data-testid="cancel-invite-button"
+                          size="large"
+                          variant="text"
+                          color="primary"
+                          onClick={() => cancel(invite)}
+                        >
+                          {t('common.cancel-invite')}
+                        </Button>
+                      </Box>
+                    ) : null}
+                  </TableCell>
+                </TableRow>
+              )
+            })}
+          </TableBody>
+        </Table>
+      </TableContainer>
       {totalCount ? (
         <TablePagination
           component="div"
@@ -177,6 +179,6 @@ export const OrgInvitesTable: React.FC<OrgInvitesTableParams> = ({ orgId }) => {
           rowsPerPageOptions={ROWS_PER_PAGE_OPTIONS}
         />
       ) : null}
-    </TableContainer>
+    </>
   )
 }
