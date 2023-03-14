63d5478f804a9e9aeebfd744525ec344b4ef8ac9
Greg Duckworth <2647926+gregoryduckworth@users.noreply.github.com>
2023-03-14 08:28:39 +0000

    chore: update booking tests to make use of page objects (#1306)

--- playwright/tests/booking/bookPrivateCourse.spec.ts
+++ playwright/tests/booking/bookPrivateCourse.spec.ts
@@ -1,51 +1,41 @@
-/* eslint-disable no-empty-pattern */
 import { test as base } from '@playwright/test'
 
 import { CourseType, InviteStatus } from '@app/types'
 
 import { deleteCourse, insertCourse } from '../../api/hasura-api'
-import { BASE_URL } from '../../constants'
 import { UNIQUE_COURSE } from '../../data/courses'
 import { Course } from '../../data/types'
 import { users } from '../../data/users'
+import { BookingPrivateCoursePage } from '../../pages/booking/BookingPrivateCoursePage'
 
 const test = base.extend<{ course: Course }>({
   course: async ({}, use) => {
     const course = UNIQUE_COURSE()
     course.type = CourseType.CLOSED
-
     course.id = await insertCourse(
       course,
       users.trainer.email,
       InviteStatus.ACCEPTED
     )
-
     await use(course)
     await deleteCourse(course.id)
   },
 })
 
 test('saves closed course booking', async ({ page, course }) => {
-  await page.goto(`${BASE_URL}/book-private-course?course_id=${course.id}`)
-  await page.waitForLoadState('domcontentloaded')
-
-  await page.locator('text=Number of course participants').type('5')
-  await page.locator('text=First Name *').type('John')
-  await page.locator('text=Last Name *').type('Doe')
-  await page.locator('text=Work email *').type('example@example.com')
-  await page.locator('text=Organisation Name *').type('Org example')
-  await page.locator('text=Phone *').type('7947492321')
-  await page.locator('text=Message (optional)').type('Message')
-
-  await page.locator('data-testid=sector-select').click()
-  await page.locator('data-testid=sector-edu').click()
-
-  await page.locator('data-testid=source-select').click()
-  await page.locator('data-testid=source-facebook').click()
-
-  await page.locator('button:has-text("Enquire now")').click()
-
-  await test
-    .expect(page.locator('text=Thank you for your enquiry'))
-    .toBeVisible()
+  const privateCoursePage = new BookingPrivateCoursePage(page)
+  await privateCoursePage.goto(`${course.id}`)
+  await privateCoursePage.setNumberOfParticipants('5')
+  await privateCoursePage.setFirstName('John')
+  await privateCoursePage.setLastName('Doe')
+  await privateCoursePage.setEmail('example@example.com')
+  await privateCoursePage.setOrganisation('Org example')
+  await privateCoursePage.setPhoneNumber('01234567890')
+  await privateCoursePage.setMessage('Message')
+  await privateCoursePage.clickSectorSelect()
+  await privateCoursePage.clickSectorEdu()
+  await privateCoursePage.clickSourceSelect()
+  await privateCoursePage.clickSourceFacebook()
+  await privateCoursePage.clickEnquireNow()
+  await privateCoursePage.checkEnquiry()
 })
--- playwright/tests/booking/enquiry.spec.ts
+++ playwright/tests/booking/enquiry.spec.ts
@@ -1,51 +1,41 @@
-/* eslint-disable no-empty-pattern */
 import { test as base } from '@playwright/test'
 
 import { CourseType, InviteStatus } from '@app/types'
 
 import { deleteCourse, insertCourse } from '../../api/hasura-api'
-import { BASE_URL } from '../../constants'
 import { UNIQUE_COURSE } from '../../data/courses'
 import { Course } from '../../data/types'
 import { users } from '../../data/users'
+import { EnquiryPage } from '../../pages/booking/EnquiryPage'
 
 const test = base.extend<{ course: Course }>({
   course: async ({}, use) => {
     const course = UNIQUE_COURSE()
     course.type = CourseType.OPEN
-
     course.id = await insertCourse(
       course,
       users.trainer.email,
       InviteStatus.ACCEPTED
     )
-
     await use(course)
     await deleteCourse(course.id)
   },
 })
 
 test('saves course enquiry', async ({ page, course }) => {
-  await page.goto(`${BASE_URL}/enquiry?course_id=${course.id}`)
-  await page.waitForLoadState('domcontentloaded')
-
-  await page.locator('text=What is your interest? *').type('Course')
-  await page.locator('text=First Name *').type('John')
-  await page.locator('text=Last Name *').type('Doe')
-  await page.locator('text=Work email *').type('example@example.com')
-  await page.locator('text=Organisation Name *').type('Org example')
-  await page.locator('text=Phone *').type('7947492321')
-  await page.locator('text=Message (optional)').type('Message')
-
-  await page.locator('data-testid=sector-select').click()
-  await page.locator('data-testid=sector-edu').click()
-
-  await page.locator('data-testid=source-select').click()
-  await page.locator('data-testid=source-facebook').click()
-
-  await page.locator('button:has-text("Enquire now")').click()
-
-  await test
-    .expect(page.locator('text=Thank you for your enquiry'))
-    .toBeVisible()
+  const enquiryPage = new EnquiryPage(page)
+  await enquiryPage.goto(`${course.id}`)
+  await enquiryPage.fillInterest('Course')
+  await enquiryPage.setFirstName('John')
+  await enquiryPage.setLastName('Doe')
+  await enquiryPage.setEmail('example@example.com')
+  await enquiryPage.setOrganisation('Org example')
+  await enquiryPage.setPhoneNumber('01234567890')
+  await enquiryPage.setMessage('Message')
+  await enquiryPage.clickSectorSelect()
+  await enquiryPage.clickSectorEdu()
+  await enquiryPage.clickSourceSelect()
+  await enquiryPage.clickSourceFacebook()
+  await enquiryPage.clickEnquireNow()
+  await enquiryPage.checkEnquiry()
 })
