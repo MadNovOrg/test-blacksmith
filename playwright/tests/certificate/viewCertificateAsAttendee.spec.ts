23402fc89827e952a0f12a6f9a2a4a907186a05c
Greg Duckworth <2647926+gregoryduckworth@users.noreply.github.com>
2023-04-05 16:48:07 +0100

    chore: split out view certificate tests in playwright (#1463)

---
+++ playwright/tests/certificate/viewCertificateAsAttendee.spec.ts
@@ -0,0 +1,49 @@
+import { test as base } from '@playwright/test'
+
+import { Grade_Enum } from '@app/generated/graphql'
+import { CourseType, InviteStatus } from '@app/types'
+
+import * as API from '../../api'
+import { FINISHED_COURSE } from '../../data/courses'
+import { Course, User } from '../../data/types'
+import { users } from '../../data/users'
+import { stateFilePath } from '../../hooks/global-setup'
+import { MyCoursesPage } from '../../pages/courses/MyCoursesPage'
+
+const test = base.extend<{ certificate: { course: Course; user: User } }>({
+  certificate: async ({}, use) => {
+    const user = users.user1
+    const course = FINISHED_COURSE()
+    course.type = CourseType.CLOSED
+    course.gradingConfirmed = true
+    course.id = await API.course.insertCourse(
+      course,
+      users.trainer.email,
+      InviteStatus.ACCEPTED
+    )
+    await API.course.insertCourseParticipants(course.id, [user])
+    await API.course.insertCourseGradingForParticipants(
+      course,
+      [user],
+      Grade_Enum.Pass
+    )
+    await API.course.insertCertificateForParticipants(course, [user])
+    await use({ course: course, user: user })
+    await API.course.deleteCourse(course.id)
+  },
+})
+
+test.use({ storageState: stateFilePath('user1') })
+
+test('attendee can view the certificate from the courses page @smoke', async ({
+  page,
+  certificate,
+}) => {
+  const myCoursesPage = new MyCoursesPage(page)
+  await myCoursesPage.goto(`${certificate.course.id}`)
+  const courseDetailsPage = await myCoursesPage.clickCourseDetailsPage(
+    certificate.course.id
+  )
+  await courseDetailsPage.clickCertification()
+  await courseDetailsPage.checkCertification('Pass')
+})
--- playwright/tests/certificate/viewCertificateAsSalesAdmin.spec.ts
+++ playwright/tests/certificate/viewCertificateAsSalesAdmin.spec.ts
@@ -9,7 +9,6 @@ import { Course, User } from '../../data/types'
 import { users } from '../../data/users'
 import { stateFilePath } from '../../hooks/global-setup'
 import { CertificationPage } from '../../pages/certificate/CertificationPage'
-import { MyCoursesPage } from '../../pages/courses/MyCoursesPage'
 
 const test = base.extend<{ certificate: { course: Course; user: User } }>({
   certificate: async ({}, use) => {
@@ -34,53 +33,17 @@ const test = base.extend<{ certificate: { course: Course; user: User } }>({
   },
 })
 
-test('ops can view the certificate on a user profile page @smoke', async ({
-  browser,
-  certificate,
-}) => {
-  const opsContext = await browser.newContext({
-    storageState: stateFilePath('ops'),
-  })
-  const opsPage = await opsContext.newPage()
-  const certificationPage = new CertificationPage(opsPage)
-  await certificationPage.goto(
-    `${certificate.user.givenName} ${certificate.user.familyName}`
-  )
-  const certificateId = await certificationPage.getFirstCertificate()
-  const profilePage = await certificationPage.clickFirstUser()
-  await profilePage.checkCertificate(certificateId)
-})
+test.use({ storageState: stateFilePath('salesAdmin') })
 
 test('sales can view the certificate on the certificate page @smoke', async ({
-  browser,
+  page,
   certificate,
 }) => {
-  const salesContext = await browser.newContext({
-    storageState: stateFilePath('salesAdmin'),
-  })
-  const salesPage = await salesContext.newPage()
-  const certificationPage = new CertificationPage(salesPage)
+  const certificationPage = new CertificationPage(page)
   await certificationPage.goto(
     `${certificate.user.givenName} ${certificate.user.familyName}`
   )
   const certificateId = await certificationPage.getFirstCertificate()
   const certificatePage = await certificationPage.clickFirstViewCertificate()
   await certificatePage.checkCertificate(certificateId)
-})
-
-test('attendee can view the certificate from the courses page @smoke', async ({
-  browser,
-  certificate,
-}) => {
-  const userContext = await browser.newContext({
-    storageState: stateFilePath('user1'),
-  })
-  const userPage = await userContext.newPage()
-  const myCoursesPage = new MyCoursesPage(userPage)
-  await myCoursesPage.goto(`${certificate.course.id}`)
-  const courseDetailsPage = await myCoursesPage.clickCourseDetailsPage(
-    certificate.course.id
-  )
-  await courseDetailsPage.clickCertification()
-  await courseDetailsPage.checkCertification('Pass')
 })
