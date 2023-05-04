f7cac8305ef6bb3ffe300051f93ae792d37284c9
Greg Duckworth <2647926+gregoryduckworth@users.noreply.github.com>
2023-05-04 11:33:49 +0100

    fix: resolve issues with permission tests in playwright (#1646)

--- playwright/tests/permissions/contacts.spec.ts
+++ playwright/tests/permissions/contacts.spec.ts
@@ -1,33 +1,45 @@
 import { expect, test } from '@playwright/test'
 
-import * as API from '../../api'
-;(
-  [
-    'anonymous',
-    'unverified',
-    'user',
-    'trainer',
-    'sales-admin',
-    'tt-ops',
-    'tt-admin',
-    'sales-representative',
-  ] as const
-).forEach(role => {
-  test(`@query it doesn't allow role ${role}  to insert a contact`, async () => {
-    const schema = await API.introspection.introspection(role)
+import { RoleName } from '@app/types'
 
+import * as API from '../../api'
+
+const insertRoles = [
+  'anonymous',
+  'unverified',
+  'user',
+  'trainer',
+  'sales-admin',
+  'tt-ops',
+  'tt-admin',
+  'sales-representative',
+] as RoleName[]
+
+insertRoles.forEach(role => {
+  test(`@query it doesn't allow role ${role} to insert a contact`, async () => {
+    const schema = await API.introspection.introspection(role)
     const insertMutation = schema.__schema.mutationType.fields.find(
       f => f.name === 'insert_xero_contact'
     )
     const insertOneMutation = schema.__schema.mutationType.fields.find(
       f => f.name === 'insert_xero_contact_one'
     )
-
     test.expect(insertMutation).toBeFalsy()
     test.expect(insertOneMutation).toBeFalsy()
   })
 })
-;(['sales-admin', 'tt-ops', 'tt-admin', 'ld'] as const).forEach(role => {
+
+const selectRoles = [
+  'sales-admin',
+  'tt-ops',
+  'tt-admin',
+  'ld',
+  'user',
+  'trainer',
+  'sales-representative',
+] as RoleName[]
+
+selectRoles.forEach(role => {
   test(`@query it allows role ${role} to select contact`, async () => {
     const schema = await API.introspection.introspection(role)
     const allowedFields = [
@@ -40,7 +52,6 @@ import * as API from '../../api'
       'phones',
       'xeroId',
     ]
-
     const contactsQuery = schema.__schema.queryType.fields.find(
       f => f.name === 'xero_contact'
     )
@@ -51,25 +62,18 @@ import * as API from '../../api'
       f => f.name === 'xero_contact'
     )
     const contactFields = contactType?.fields.map(f => f.name)
-
     const hasAllowedFields = allowedFields?.every(f =>
       contactFields?.includes(f)
     )
-
     expect(contactQuery).toBeTruthy()
     expect(contactsQuery).toBeTruthy()
     expect(hasAllowedFields).toBe(true)
   })
 })
-;(
-  [
-    'unverified',
-    'anonymous',
-    'user',
-    'trainer',
-    'sales-representative',
-  ] as const
-).forEach(role => {
+
+const cannotSelectRoles = ['unverified', 'anonymous'] as RoleName[]
+
+cannotSelectRoles.forEach(role => {
   test(`@query doesn't allow role ${role} to select a contact`, async () => {
     const schema = await API.introspection.introspection(role)
     const contactsQuery = schema.__schema.queryType.fields.find(
--- playwright/tests/permissions/invoices.spec.ts
+++ playwright/tests/permissions/invoices.spec.ts
@@ -1,33 +1,45 @@
 import { expect, test } from '@playwright/test'
 
+import { RoleName } from '@app/types'
+
 import * as API from '../../api'
-;(
-  [
-    'anonymous',
-    'unverified',
-    'user',
-    'trainer',
-    'sales-admin',
-    'tt-ops',
-    'tt-admin',
-    'sales-representative',
-  ] as const
-).forEach(role => {
+
+const insertRoles = [
+  'anonymous',
+  'unverified',
+  'user',
+  'trainer',
+  'sales-admin',
+  'tt-ops',
+  'tt-admin',
+  'sales-representative',
+] as RoleName[]
+
+insertRoles.forEach(role => {
   test(`@query it doesn't allow role ${role}  to insert an invoice`, async () => {
     const schema = await API.introspection.introspection(role)
-
     const insertMutation = schema.__schema.mutationType.fields.find(
       f => f.name === 'insert_xero_invoice'
     )
     const insertOneMutation = schema.__schema.mutationType.fields.find(
       f => f.name === 'insert_xero_invoice_one'
     )
-
     test.expect(insertMutation).toBeFalsy()
     test.expect(insertOneMutation).toBeFalsy()
   })
 })
-;(['sales-admin', 'tt-ops', 'tt-admin', 'ld'] as const).forEach(role => {
+
+const selectRoles = [
+  'sales-admin',
+  'tt-ops',
+  'tt-admin',
+  'ld',
+  'user',
+  'trainer',
+  'sales-representative',
+] as RoleName[]
+
+selectRoles.forEach(role => {
   test(`@query it allows role ${role} to select invoice`, async () => {
     const schema = await API.introspection.introspection(role)
     const allowedFields = [
@@ -49,7 +61,6 @@ import * as API from '../../api'
       'xeroContactId',
       'xeroId',
     ]
-
     const invoicesQuery = schema.__schema.queryType.fields.find(
       f => f.name === 'xero_invoice'
     )
@@ -60,25 +71,18 @@ import * as API from '../../api'
       f => f.name === 'xero_invoice'
     )
     const invoiceFields = invoiceType?.fields.map(f => f.name)
-
     const hasAllowedFields = allowedFields?.every(f =>
       invoiceFields?.includes(f)
     )
-
     expect(invoiceQuery).toBeTruthy()
     expect(invoicesQuery).toBeTruthy()
     expect(hasAllowedFields).toBe(true)
   })
 })
-;(
-  [
-    'unverified',
-    'anonymous',
-    'user',
-    'trainer',
-    'sales-representative',
-  ] as const
-).forEach(role => {
+
+const cannotSelectRoles = ['unverified', 'anonymous'] as RoleName[]
+
+cannotSelectRoles.forEach(role => {
   test(`@query doesn't allow role ${role} to select an invoice`, async () => {
     const schema = await API.introspection.introspection(role)
     const invoicesQuery = schema.__schema.queryType.fields.find(
