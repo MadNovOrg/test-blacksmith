c6f863012e4325a21c5c029f7d3c6d8882d32313
Salman Mitha <salmanmitha@gmail.com>
2023-05-15 16:24:18 +0530

    fix: cert changelog permissions (#1715)

--- src/queries/certificate/revoke-certificate.tsx
+++ src/queries/certificate/revoke-certificate.tsx
@@ -2,19 +2,19 @@ import { gql } from 'graphql-request'
 
 export const MUTATION = gql`
   mutation RevokeCert($id: uuid!, $participantId: uuid!, $payload: jsonb!) {
-    revoked: update_course_certificate_by_pk(
-      pk_columns: { id: $id }
-      _set: { isRevoked: true }
-    ) {
-      id
-    }
-
     insertChangeLog: insert_course_certificate_changelog_one(
       object: {
         participantId: $participantId
         type: REVOKED
         payload: $payload
       }
+    ) {
+      id
+    }
+
+    revoked: update_course_certificate_by_pk(
+      pk_columns: { id: $id }
+      _set: { isRevoked: true }
     ) {
       id
     }
