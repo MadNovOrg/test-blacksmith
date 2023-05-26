1e6cf4c0d7570c129ae55bc2bd8844c0ed107361
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2023-05-26 11:36:20 +0200

    fix(TTHP-1485): bild trainer ratio (#1849)
    * 1 lead trainer always, assist is required only after certain number of participants
    
    * change test titles to reflect actual functionality

--- src/util/trainerRatio/trainerRatio.bild.test.ts
+++ src/util/trainerRatio/trainerRatio.bild.test.ts
@@ -9,7 +9,7 @@ import {
 } from './trainerRatio.bild'
 
 describe('Bild trainer ratios', () => {
-  it('requires 1 lead regardless of participants and 1 assist per next 12 participants after 12 participants have been registered', () => {
+  it('requires 1 lead regardless of participants and 1 assist per next 12 participants after 12 participants have been registered for a conversion course', () => {
     const criteria: BildRatioCriteria = {
       isConversion: true,
       numberParticipants: 11,
@@ -30,7 +30,7 @@ describe('Bild trainer ratios', () => {
     ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead and 1 assist per 12 participants given a course is ${Course_Level_Enum.BildIntermediateTrainer} and is not reaccreditation`, () => {
+  it(`requires 1 lead regardless of participants and 1 assist per next 12 participants after 12 participants have been registered given a course is ${Course_Level_Enum.BildIntermediateTrainer} and is not reaccreditation`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -51,38 +51,38 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 12,
       })
+    ).toEqual(toRange(0))
+
+    expect(
+      getRequiredLeadsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
+    ).toEqual(toRange(1))
+
+    expect(
+      getRequiredAssistantsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
     ).toEqual(toRange(1))
 
     expect(
       getRequiredLeadsBild({
         ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredAssistantsBild({
-        ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredLeadsBild({
-        ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
         ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead per 12 participants and 1 assist above 12 participants given a course is ${Course_Level_Enum.BildIntermediateTrainer} and is reaccreditation`, () => {
+  it(`requires 1 lead regardless of participant and 1 assist per next 12 participants after 12 participants have been registered given a course is ${Course_Level_Enum.BildIntermediateTrainer} and is reaccreditation`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -110,7 +110,7 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 24,
       })
-    ).toEqual(toRange(2))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
@@ -124,7 +124,7 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
@@ -134,7 +134,7 @@ describe('Bild trainer ratios', () => {
     ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead and 1 assist per 12 participants given a course is ${Course_Level_Enum.BildAdvancedTrainer} regardless of reaccreditation`, () => {
+  it(`requires 1 lead regardless of participants and 1 assist per next 12 participants after 12 participants have been registered given a course is ${Course_Level_Enum.BildAdvancedTrainer} regardless of reaccreditation`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -165,7 +165,7 @@ describe('Bild trainer ratios', () => {
         isReaccreditation: false,
         numberParticipants: 12,
       })
-    ).toEqual(toRange(1))
+    ).toEqual(toRange(0))
 
     expect(
       getRequiredAssistantsBild({
@@ -173,38 +173,38 @@ describe('Bild trainer ratios', () => {
         isReaccreditation: true,
         numberParticipants: 12,
       })
+    ).toEqual(toRange(0))
+
+    expect(
+      getRequiredLeadsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
+    ).toEqual(toRange(1))
+
+    expect(
+      getRequiredAssistantsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
     ).toEqual(toRange(1))
 
     expect(
       getRequiredLeadsBild({
         ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredAssistantsBild({
-        ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredLeadsBild({
-        ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
         ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead and 1 assist per 12 participants given a course is ${Course_Level_Enum.BildRegular} and ${BildStrategies.Primary} is the only selected strategy`, () => {
+  it(`requires 1 lead regardless of participants and 1 assist per next 12 participants after 12 participants have been registered given a course is ${Course_Level_Enum.BildRegular} and ${BildStrategies.Primary} is the only selected strategy`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -225,38 +225,38 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 12,
       })
+    ).toEqual(toRange(0))
+
+    expect(
+      getRequiredLeadsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
+    ).toEqual(toRange(1))
+
+    expect(
+      getRequiredAssistantsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
     ).toEqual(toRange(1))
 
     expect(
       getRequiredLeadsBild({
         ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredAssistantsBild({
-        ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredLeadsBild({
-        ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
         ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead and 1 assist per 12 participants given a course is ${Course_Level_Enum.BildRegular} and all strategies except ${BildStrategies.RestrictiveTertiaryAdvanced} is selected`, () => {
+  it(`requires 1 lead regardless of participant and 1 assist per next 12 participants after 12 participants have been registered given a course is ${Course_Level_Enum.BildRegular} and all strategies except ${BildStrategies.RestrictiveTertiaryAdvanced} is selected`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -282,38 +282,38 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 12,
       })
+    ).toEqual(toRange(0))
+
+    expect(
+      getRequiredLeadsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
+    ).toEqual(toRange(1))
+
+    expect(
+      getRequiredAssistantsBild({
+        ...criteria,
+        numberParticipants: 24,
+      })
     ).toEqual(toRange(1))
 
     expect(
       getRequiredLeadsBild({
         ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredAssistantsBild({
-        ...criteria,
-        numberParticipants: 24,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredLeadsBild({
-        ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
         ...criteria,
         numberParticipants: 36,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(2))
   })
 
-  it(`requires 1 lead and 1 assist per 8 participants given a course is ${Course_Level_Enum.BildRegular} and all strategies have been selected`, () => {
+  it(`requires 1 lead regardless of participants and 1 assist per next 8 participants after 8 participants have been registered given a course is ${Course_Level_Enum.BildRegular} and all strategies have been selected`, () => {
     const criteria: BildRatioCriteria = {
       isConversion: false,
       numberParticipants: 25,
@@ -340,34 +340,34 @@ describe('Bild trainer ratios', () => {
         ...criteria,
         numberParticipants: 8,
       })
+    ).toEqual(toRange(0))
+
+    expect(
+      getRequiredLeadsBild({
+        ...criteria,
+        numberParticipants: 16,
+      })
+    ).toEqual(toRange(1))
+
+    expect(
+      getRequiredAssistantsBild({
+        ...criteria,
+        numberParticipants: 16,
+      })
     ).toEqual(toRange(1))
 
     expect(
       getRequiredLeadsBild({
         ...criteria,
-        numberParticipants: 16,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredAssistantsBild({
-        ...criteria,
-        numberParticipants: 16,
-      })
-    ).toEqual(toRange(2))
-
-    expect(
-      getRequiredLeadsBild({
-        ...criteria,
         numberParticipants: 24,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(1))
 
     expect(
       getRequiredAssistantsBild({
         ...criteria,
         numberParticipants: 24,
       })
-    ).toEqual(toRange(3))
+    ).toEqual(toRange(2))
   })
 })
--- src/util/trainerRatio/trainerRatio.bild.ts
+++ src/util/trainerRatio/trainerRatio.bild.ts
@@ -8,47 +8,17 @@ export type BildRatioCriteria = {
   numberParticipants: number
 }
 
-export function getRequiredLeadsBild(criteria: BildRatioCriteria): {
+export function getRequiredLeadsBild(_criteria: BildRatioCriteria): {
   min: number
   max: number
 } {
-  const count = 1
-
-  const withoutAdvanced =
-    criteria.level === CourseLevel.BildRegular &&
-    criteria.strategies.length >= 1 &&
-    !criteria.strategies.includes(BildStrategies.RestrictiveTertiaryAdvanced)
-
-  const allStrategiesSelected =
-    criteria.level === CourseLevel.BildRegular &&
-    Object.keys(BildStrategies).length === criteria.strategies.length
-
-  if (criteria.isConversion) {
-    return toRange(1)
-  }
-
-  if (
-    [
-      CourseLevel.BildIntermediateTrainer,
-      CourseLevel.BildAdvancedTrainer,
-    ].includes(criteria.level) ||
-    withoutAdvanced
-  ) {
-    return toRange(Math.ceil(criteria.numberParticipants / 12))
-  }
-
-  if (allStrategiesSelected) {
-    return toRange(Math.ceil(criteria.numberParticipants / 8))
-  }
-
-  return toRange(count)
+  return toRange(1)
 }
 
 export function getRequiredAssistantsBild(criteria: BildRatioCriteria): {
   min: number
   max: number
 } {
-  const count = 0
   const withoutAdvanced =
     criteria.level === CourseLevel.BildRegular &&
     criteria.strategies.length >= 1 &&
@@ -63,27 +33,25 @@ export function getRequiredAssistantsBild(criteria: BildRatioCriteria): {
   }
 
   if (
-    criteria.level === CourseLevel.BildIntermediateTrainer &&
-    criteria.isReaccreditation &&
+    (withoutAdvanced ||
+      [
+        CourseLevel.BildIntermediateTrainer,
+        CourseLevel.BildAdvancedTrainer,
+      ].includes(criteria.level)) &&
     criteria.numberParticipants > 12
   ) {
     return toRange(Math.abs(Math.ceil((criteria.numberParticipants - 12) / 12)))
   }
 
   if (
-    (criteria.level === CourseLevel.BildIntermediateTrainer &&
-      !criteria.isReaccreditation) ||
-    criteria.level === CourseLevel.BildAdvancedTrainer ||
-    withoutAdvanced
+    allStrategiesSelected &&
+    criteria.level === CourseLevel.BildRegular &&
+    criteria.numberParticipants > 8
   ) {
-    return toRange(Math.ceil(criteria.numberParticipants / 12))
+    return toRange(Math.abs(Math.ceil((criteria.numberParticipants - 8) / 8)))
   }
 
-  if (allStrategiesSelected) {
-    return toRange(Math.ceil(criteria.numberParticipants / 8))
-  }
-
-  return toRange(count)
+  return toRange(0)
 }
 
 export function toRange(count: number) {
