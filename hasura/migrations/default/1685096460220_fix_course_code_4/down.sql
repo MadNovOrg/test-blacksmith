8d225a95de778acaeb875b334f57c7082f46bcb8
Salman Mitha <salmanmitha@gmail.com>
2023-05-26 16:31:53 +0530

    fix: course code (#1851)

---
+++ hasura/migrations/default/1685096460220_fix_course_code_4/down.sql
@@ -0,0 +1,3 @@
+UPDATE course_level_prefix SET prefix = 'BILD_REGULAR' WHERE name = 'BILD';
+UPDATE course_level_prefix SET prefix = 'BILD_INTERMEDIATE_TRAINER' WHERE name = 'BILD-INT';
+UPDATE course_level_prefix SET prefix = 'BILD_ADVANCED_TRAINER' WHERE name = 'BILD-ADV';
