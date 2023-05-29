5dad85252ab06a0855d903cbe2cd8885d58950e4
Danijel Maksimovic <danijel.maksimovic@nearform.com>
2023-05-29 16:40:27 +0200

    change bild course level prefixes, don't include bild strategies if not bild regular level in course code (#1871)

---
+++ hasura/migrations/default/1685369908459_bild_course_level_prefix/down.sql
@@ -0,0 +1,3 @@
+UPDATE course_level_prefix SET prefix = 'BILD' WHERE name = 'BILD_REGULAR';
+UPDATE course_level_prefix SET prefix = 'BILD-INT' WHERE name = 'BILD_INTERMEDIATE_TRAINER';
+UPDATE course_level_prefix SET prefix = 'BILD-ADV' WHERE name = 'BILD_ADVANCED_TRAINER';
