00840e68b34d822315033861f58d18a19ecc8490
Grzegorz Ilach <grzegorz.ilach@nearform.com>
2023-06-08 09:35:25 +0200

    fix: failing test (#1923)

--- src/components/FilterCourseLevel/FilterCourseLevel.test.tsx
+++ src/components/FilterCourseLevel/FilterCourseLevel.test.tsx
@@ -10,10 +10,10 @@ describe('component: FilterCourseLevel', () => {
     render(<FilterCourseLevel onChange={onChange} />)
 
     await waitFor(() => {
-      expect(screen.getByText('Level')).toBeVisible()
+      expect(screen.getByText('Course level')).toBeVisible()
     })
 
-    await userEvent.click(screen.getByText('Level'))
+    await userEvent.click(screen.getByText('Course level'))
     await userEvent.click(screen.getByText('Advanced Trainer'))
 
     expect(onChange).toHaveBeenCalledWith(['ADVANCED_TRAINER'])
