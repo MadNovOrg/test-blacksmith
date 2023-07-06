158031b9d9ec9d69422a767396bf98849b176b26
Alexei Gaidulean <Alexei.Gaidulean@teamteach.co.uk>
2023-07-06 23:29:00 +0300

    chore: improve hasura related workflow (#2156)
    * impr: add more metadata related commands, group scripts
    
    * fix: enable hasura GQL intellisense

--- apollo.config.js
+++ apollo.config.js
@@ -1,8 +1,12 @@
+/**
+ * Enabling `apollographql.vscode-apollo` will add GraphQL intellisense
+ * @see https://hasura.io/docs/latest/resources/visual-studio-code/
+ */
 module.exports = {
   client: {
     service: {
       name: 'my-graphql-app',
-      url: 'http://localhost:3000/v1/graphql',
+      url: 'http://localhost:8080/v1/graphql',
       headers: {
         'x-hasura-admin-secret': 'tth-hasura-key',
       },
