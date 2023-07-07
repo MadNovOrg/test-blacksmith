925c4cebb1fa21a7cd6ec138ead3446eea510502
Salman Mitha <salmanmitha@gmail.com>
2023-07-07 15:20:56 +0530

    chore: migrations to optimize db schema and upgrade hasura (#2138)
    * chore: add indexes in db for performace
    
    * chore: upgrade hasura
    
    chore: squash migration

---
+++ hasura/migrations/default/1688487087293_add-indexes-for-perf/down.sql
@@ -0,0 +1,28 @@
+
+DROP INDEX IF EXISTS "public"."course_participant_invite_id";
+
+DROP INDEX IF EXISTS "public"."course_invites_course_id";
+
+
+DROP INDEX IF EXISTS "public"."course_schedule_end";
+
+DROP INDEX IF EXISTS "public"."course_schedule_start";
+
+
+DROP INDEX IF EXISTS "public"."course_schedule_venue_id";
+
+DROP INDEX IF EXISTS "public"."course_cancellation_request_course_id";
+
+DROP INDEX IF EXISTS "public"."course_schedule_course_id";
+
+DROP INDEX IF EXISTS "public"."waitlist_course_id";
+
+DROP INDEX IF EXISTS "public"."course_trainer_profile_id";
+
+DROP INDEX IF EXISTS "public"."course_organization_id";
+
+DROP INDEX IF EXISTS "public"."course_module_course_id";
+
+DROP INDEX IF EXISTS "public"."course_trainer_course_id";
+
+DROP INDEX IF EXISTS "public"."course_participant_course_id";
