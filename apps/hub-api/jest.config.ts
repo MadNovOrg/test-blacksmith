99e349f1127a6051119c2a22e30e575d26b60743
Alexei Gaidulean <Alexei.Gaidulean@teamteach.co.uk>
2023-09-01 19:57:52 +0300

    chore (TTHP-2510): create stub fastify project (#2500)
    * chore (TTHP-2510): create stub fastify project
    
    * revert: extensions.json

---
+++ apps/hub-api/jest.config.ts
@@ -0,0 +1,13 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  displayName: 'hub-api',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
+  },
+  moduleFileExtensions: ['ts', 'js'],
+}
+
+export default config
