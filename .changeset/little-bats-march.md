a37da3cdcc8a4e96f9fb27dc86fdf842b5c3b91e
vadimcernev <145479757+vadimcernev@users.noreply.github.com>
2023-11-06 10:28:00 +0200

    fix: (TTHP-2944) xero_code for advanced trainer course level (#2931)

---
+++ .changeset/little-bats-march.md
@@ -0,0 +1,5 @@
+---
+'@teamteach/hub': patch
+---
+
+update xero_code value for level ADVANCED_TRAINER type OPEN and reacreditation fase
---
+++ hasura/migrations/default/1699019372320_run_sql_migration/down.sql
@@ -0,0 +1,3 @@
+UPDATE course_pricing
+SET xero_code = 'INT.RE.OP'
+WHERE type = 'OPEN' AND level = 'ADVANCED_TRAINER' AND reaccreditation = false;
