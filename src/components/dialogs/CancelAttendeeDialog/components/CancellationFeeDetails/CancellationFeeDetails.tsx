b37754592a1bcdfc7e29c73bfacb69c132bbf7c2
madalin-novac <135691767+madalin-novac@users.noreply.github.com>
2023-11-30 15:05:52 +0200

    fix (tthp-3319): missed link to update (#3158)
    Co-authored-by: Marcel Novac <marcel.novac@Madalins-MacBook-Pro.local>

--- src/components/dialogs/CancelAttendeeDialog/components/CancellationFeeDetails/CancellationFeeDetails.tsx
+++ src/components/dialogs/CancelAttendeeDialog/components/CancellationFeeDetails/CancellationFeeDetails.tsx
@@ -107,7 +107,7 @@ export const CancellationFeeDetails: React.FC<
                   rel="noreferrer"
                   href={`${
                     import.meta.env.VITE_BASE_WORDPRESS_URL
-                  }/terms-of-business/`}
+                  }/policies-procedures/terms-of-business/`}
                 />
               ),
             }}
