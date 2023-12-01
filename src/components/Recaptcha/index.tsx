34cd9e13f21f121261986af0eb0f1829209bbd79
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2023-12-01 11:12:52 +0200

    fix/TTHP-3212: check if grecaptcha is null or undefined (#3157)
    Co-authored-by: ionut-vidrighin <132457599+ionut-vidrighin@users.noreply.github.com>

--- src/components/Recaptcha/index.tsx
+++ src/components/Recaptcha/index.tsx
@@ -24,22 +24,24 @@ export const Recaptcha: React.FC<Props> = ({
   const widgetId = useRef<number>()
 
   useMount(() => {
-    window.grecaptcha.enterprise.ready(async () => {
-      if (typeof widgetId.current !== 'undefined') {
-        return
-      }
+    if (window.grecaptcha !== null && window.grecaptcha !== undefined) {
+      window.grecaptcha.enterprise?.ready(async () => {
+        if (typeof widgetId.current !== 'undefined') {
+          return
+        }
 
-      widgetId.current = window.grecaptcha.enterprise.render(id.current, {
-        sitekey: import.meta.env.VITE_RECAPTCHA_KEY,
-        callback: token => {
-          console.log(token)
-          onSuccess(token)
-        },
-        'expired-callback': onExpired,
-        'error-callback': onError,
-        action: String(action),
+        widgetId.current = window.grecaptcha.enterprise?.render(id.current, {
+          sitekey: import.meta.env.VITE_RECAPTCHA_KEY,
+          callback: token => {
+            console.log(token)
+            onSuccess(token)
+          },
+          'expired-callback': onExpired,
+          'error-callback': onError,
+          action: String(action),
+        })
       })
-    })
+    }
   })
 
   return <div id={id.current} />
