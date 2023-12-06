da6394e9555d0b08ec3e0e16d8fbe0831efab0a0
madalin-novac <135691767+madalin-novac@users.noreply.github.com>
2023-12-06 11:24:03 +0200

    chore: add course pricing schedules seeds (#3200)
    Co-authored-by: Marcel Novac <marcel.novac@Madalins-MacBook-Pro.local>

---
+++ hasura/seeds/default/dev/026_course_pricing_schedule.sql
@@ -0,0 +1,9 @@
+INSERT INTO course_pricing_schedule (course_pricing_id, effective_from, effective_to, price_amount, price_currency)
+SELECT
+    id,
+    '2023-01-01',
+    '2024-12-31',
+    price_amount,
+    price_currency
+FROM
+    course_pricing;
