19aada9474da66f8c50897e4b91dc20d72bd7e5a
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2023-12-20 10:19:48 +0200

    Feat/tthp 2782 open course builder 3day srt (#3280)
    * feat/TTHP-2782: Initial commit
    
    * feat/TTHP-2782: Add mandatory modules in 3 Day SRT open course.
    
    * feat/TTHP-2782: fix comments
    
    * feat/TTHP-2782: correct column name
    
    * feat/TTHP-2782: remove console logs
    
    * feat/TTHP-2782: create submodule table and threat them as modules
    
    * feat/TTHP-2782: rename submodule Click to Clock

---
+++ hasura/migrations/default/1702997349700_rename_click_submodule_name_to_clock/down.sql
@@ -0,0 +1,3 @@
+Update submodule
+Set name = 'Click'
+Where name = 'Clock' AND course_level = 'THREE_DAY_SAFETY_RESPONSE_TRAINER'
---
+++ hasura/seeds/default/dev/028_update_click_submodule_name.sql
@@ -0,0 +1,3 @@
+Update submodule
+Set name = 'Clock'
+Where name = 'Click' AND course_level = 'THREE_DAY_SAFETY_RESPONSE_TRAINER'
