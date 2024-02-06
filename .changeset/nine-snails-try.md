ca4924446c10eca3a7339ab166f1b3635f881ba4
ionut-vidrighin <132457599+ionut-vidrighin@users.noreply.github.com>
2024-02-06 11:37:39 +0200

    feat (TTHP-3089) updating prices for 3 Day SRT course level (#3471)

---
+++ .changeset/nine-snails-try.md
@@ -0,0 +1,5 @@
+---
+'@teamteach/hub': minor
+---
+
+Updating prices for 3 Day SRT course level
---
+++ hasura/migrations/default/1706777131792_update_3daySRT_price/up.sql
@@ -0,0 +1,24 @@
+
+UPDATE public.course_pricing
+SET price_currency = 'EUR'
+WHERE id = '1eafc28f-497b-434e-ba74-caedfc3d12a2';
+
+UPDATE public.course_pricing
+SET price_amount = '425'
+WHERE id = 'ad6e62c0-fc49-4cae-b6f7-c4d75c37a531';
+
+UPDATE public.course_pricing
+SET price_currency = 'EUR'
+WHERE id = 'ad6e62c0-fc49-4cae-b6f7-c4d75c37a531';
+
+UPDATE public.course_pricing_schedule
+SET price_currency = 'EUR'
+WHERE id = '882922b4-4de4-4385-93a2-d5d3c1d8ed89';
+
+UPDATE public.course_pricing_schedule
+SET price_currency = 'EUR'
+WHERE id = 'f61c8284-25e2-4d8c-9968-167102402eba';
+
+UPDATE public.course_pricing_schedule
+SET price_amount = '425'
+WHERE id = 'f61c8284-25e2-4d8c-9968-167102402eba';
