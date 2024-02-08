78059831f8d0fa78acbe1d25d638cf34e2a4f237
ionut-vidrighin <132457599+ionut-vidrighin@users.noreply.github.com>
2024-02-08 09:31:25 +0200

    feat (TTHP-3089) Updating price for 3 Day SRT (#3506)

---
+++ hasura/migrations/default/1707316091722_update price/down.sql
@@ -0,0 +1,3 @@
+UPDATE public.course_pricing_schedule
+SET price_amount = '600'
+WHERE course_pricing_id = (SELECT id FROM public.course_pricing WHERE level = 'THREE_DAY_SAFETY_RESPONSE_TRAINER' AND reaccreditation = true limit 1);
