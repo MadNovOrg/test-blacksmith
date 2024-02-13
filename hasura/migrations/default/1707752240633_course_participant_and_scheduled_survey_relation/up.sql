2620c4ccd41ca6fd3cfe4e8bc6b4057765c46b4c
IonMereuta <139231999+IonMereuta@users.noreply.github.com>
2024-02-13 17:49:53 +0200

    feat/TTHP-3537 Handle historic participants for training surveys (#3544)
    * feat/TTHP-3537 Handle historic participants for training surveys
    
    * feat/TTHP-3537 Handle historic participants for training surveys
    
    * feat/TTHP-3537 Handle historic participants for training surveys

---
+++ hasura/migrations/default/1707752240633_course_participant_and_scheduled_survey_relation/up.sql
@@ -0,0 +1,10 @@
+
+alter table "public"."about_training_email_scheduled_events"
+  add constraint "about_training_email_scheduled_events_course_participant_id_"
+  foreign key ("course_participant_id")
+  references "public"."course_participant"
+  ("id") on update cascade on delete cascade;
+
+alter table "public"."about_training_email_scheduled_events" drop constraint "about_training_email_schedule_course_participant_id_event_i_key";
+
+alter table "public"."about_training_email_scheduled_events" add constraint "about_training_email_scheduled_events_course_participant_id_key" unique ("course_participant_id");
