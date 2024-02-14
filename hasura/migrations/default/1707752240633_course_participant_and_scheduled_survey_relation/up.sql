f98baebffb0106851ae3e1cad365a10d1ae43a34
IonMereuta <139231999+IonMereuta@users.noreply.github.com>
2024-02-14 14:04:08 +0200

    fix/Remove duplicates in training surveys events table (#3549)
    * fix/Remove duplicates in training surveys events table
    
    * fix/Remove duplicates in training surveys events table

--- hasura/migrations/default/1707752240633_course_participant_and_scheduled_survey_relation/up.sql
+++ hasura/migrations/default/1707752240633_course_participant_and_scheduled_survey_relation/up.sql
@@ -5,6 +5,18 @@ where
      from "public"."course_participant" as course_participant
      where course_participant.id = about_training_email_scheduled_events.course_participant_id) = 0;
 
+with duplicates_cte as
+  (select *,
+          row_number() over (partition by about_training_email_scheduled_events.course_participant_id
+                             order by id) as row_num
+   from "public"."about_training_email_scheduled_events" as about_training_email_scheduled_events)
+delete
+from "public"."about_training_email_scheduled_events"
+where id in
+    (select id
+     from duplicates_cte
+     where row_num > 1);
+
 alter table "public"."about_training_email_scheduled_events"
   add constraint "about_training_email_scheduled_events_course_participant_id_"
   foreign key ("course_participant_id")
