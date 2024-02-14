fde4723dc146acbba5f27cfe5cd6c7b673489c84
IonMereuta <139231999+IonMereuta@users.noreply.github.com>
2024-02-14 11:24:00 +0200

    fix/TTHP-3537 Fix (#3548)
    * fix/TTHP-3537 Fix
    
    * Update up.sql

--- hasura/migrations/default/1707752240633_course_participant_and_scheduled_survey_relation/up.sql
+++ hasura/migrations/default/1707752240633_course_participant_and_scheduled_survey_relation/up.sql
@@ -1,9 +1,15 @@
+delete
+from "public"."about_training_email_scheduled_events" as about_training_email_scheduled_events
+where
+    (select count(*)
+     from "public"."course_participant" as course_participant
+     where course_participant.id = about_training_email_scheduled_events.course_participant_id) = 0;
 
 alter table "public"."about_training_email_scheduled_events"
   add constraint "about_training_email_scheduled_events_course_participant_id_"
   foreign key ("course_participant_id")
   references "public"."course_participant"
-  ("id") on update no action on delete no action;
+  ("id") on update cascade on delete cascade;
 
 alter table "public"."about_training_email_scheduled_events" drop constraint "about_training_email_schedule_course_participant_id_event_i_key";
 
