cf003a062d073797ef49e617526d7a2dc6c09d2e
IonMereuta <139231999+IonMereuta@users.noreply.github.com>
2024-04-01 14:29:31 +0300

    feat/TTHP- Level 1 MVA course codes (#3809)
    * feat/TTHP-2805 Level 1 MVA course codes
    
    * feat/TTHP-2805 Update pricing values

---
+++ hasura/migrations/default/1711704000390_insert_into_public_course_pricing/up.sql
@@ -0,0 +1,6 @@
+INSERT INTO "public"."course_pricing"("blended", "reaccreditation", "level", "price_currency", "type", "xero_code", "created_at", "updated_at", "price_amount") VALUES
+(false, false, E'LEVEL_1_MVA', E'GBP', E'CLOSED', E'LEVEL1.MVA.CL', E'2024-03-29T09:20:00.378356+00:00', E'2024-03-29T09:20:00.378356+00:00', 0),
+(true, false, E'LEVEL_1_MVA', E'GBP', E'CLOSED', E'LEVEL1.MVA.CL', E'2024-03-29T09:20:00.378356+00:00', E'2024-03-29T09:20:00.378356+00:00', 0),
+(false, true, E'LEVEL_1_MVA', E'GBP', E'CLOSED', E'LEVEL1.MVA.RE.CL', E'2024-03-29T09:20:00.378356+00:00', E'2024-03-29T09:20:00.378356+00:00', 0),
+(true, true, E'LEVEL_1_MVA', E'GBP', E'CLOSED', E'LEVEL1.MVA.RE.CL', E'2024-03-29T09:20:00.378356+00:00', E'2024-03-29T09:20:00.378356+00:00', 0);
+
