19e90b3e988bcecf5e1b54144db8c72e1dbb1911
IonMereuta <139231999+IonMereuta@users.noreply.github.com>
2024-05-28 15:27:52 +0300

    feat/TTHP-3962 Restrict trainer from accept cancelled and declined courses (#4089)

---
+++ .changeset/fifty-grapes-greet.md
@@ -0,0 +1,5 @@
+---
+'@teamteach/hub': patch
+---
+
+Do not allow trainers to accept Cancelled or Declined courses
--- src/pages/trainer-pages/MyCourses/hooks/useActionableCourses.ts
+++ src/pages/trainer-pages/MyCourses/hooks/useActionableCourses.ts
@@ -142,7 +142,13 @@ export default function useActionableCourses({
           status: { _eq: Course_Invite_Status_Enum.Pending },
           profile_id: { _eq: profile?.id },
         },
-        status: { _neq: Course_Status_Enum.ExceptionsApprovalPending },
+        status: {
+          _nin: [
+            Course_Status_Enum.Cancelled,
+            Course_Status_Enum.Declined,
+            Course_Status_Enum.ExceptionsApprovalPending,
+          ],
+        },
       })
     } else {
       conditions.push(statusCondition)
