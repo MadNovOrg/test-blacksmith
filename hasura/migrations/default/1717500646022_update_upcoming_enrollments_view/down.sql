bea2fbbe8629e0a3134f94f5973c1c6bbde95fe1
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2024-06-06 12:35:54 +0300

    fix/TTHP-3934: Upcoming enrollments are courses in present or in the future. (#4121)

---
+++ hasura/migrations/default/1717500646022_update_upcoming_enrollments_view/down.sql
@@ -0,0 +1,15 @@
+DROP VIEW IF EXISTS upcoming_enrollments;
+
+CREATE VIEW upcoming_enrollments AS
+SELECT profile.id          as profile_id,
+       course.course_level as course_level,
+       schedule.start      as schedule_start,
+       course.id           as course_id,
+       org.id              as org_id,
+       org.name            as org_name
+FROM public.profile profile
+         JOIN public.course_participant cp ON cp.profile_id = profile.id
+         JOIN public.course course ON course.id = cp.course_id
+         JOIN public.organization org ON course.organization_id = org.id
+         JOIN public.course_schedule schedule ON schedule.course_id = course.id
+WHERE schedule.start > NOW();
