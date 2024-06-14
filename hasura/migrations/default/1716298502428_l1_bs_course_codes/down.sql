13d8ae170dae53e8efb9c8cf43e0cff0ce502192
AlbertBadea <162566544+AlbertBadea@users.noreply.github.com>
2024-06-14 11:28:57 +0300

    feat (TTHP-3481): Add Indirect L1 BS course codes (#4057)
    * feat (TTHP-3481): Add Indirect L1 BS course codes
    
    * squash migrations
    
    ---------
    
    Co-authored-by: Marcel Novac <marcel.novac@Madalins-MacBook-Pro.local>
    Co-authored-by: madalin-novac <135691767+madalin-novac@users.noreply.github.com>

---
+++ hasura/migrations/default/1716298502428_l1_bs_course_codes/down.sql
@@ -0,0 +1,4 @@
+
+DELETE FROM "public"."course_pricing" WHERE "id" = 'bd1b9d4f-8cc2-4b90-87b3-7edd380d72d8';
+
+DELETE FROM "public"."course_pricing" WHERE "id" = '23a9d3a4-f221-4550-8ee8-7eb564185d02';
---
+++ hasura/migrations/default/1716298502428_l1_bs_course_codes/up.sql
@@ -0,0 +1,4 @@
+
+INSERT INTO "public"."course_pricing"("id", "type", "level", "blended", "reaccreditation", "price_amount", "price_currency", "xero_code", "created_at", "updated_at") VALUES (E'23a9d3a4-f221-4550-8ee8-7eb564185d02', E'INDIRECT', E'LEVEL_1_BS', false, false, 0, E'GBP', E'L1BS.F.INDR', E'2024-05-21T13:34:43.58547+00:00', E'2024-05-21T13:34:43.58547+00:00');
+
+INSERT INTO "public"."course_pricing"("id", "type", "level", "blended", "reaccreditation", "price_amount", "price_currency", "xero_code", "created_at", "updated_at") VALUES (E'bd1b9d4f-8cc2-4b90-87b3-7edd380d72d8', E'INDIRECT', E'LEVEL_1_BS', false, true, 0, E'GBP', E'L1BS.RE.F.INDR', E'2024-05-21T13:35:02.420012+00:00', E'2024-05-21T13:35:02.420012+00:00');
