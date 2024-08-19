77b902d4ef77da1ce547ae9a1251e861c4fceafa
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2024-08-19 08:44:27 +0300

    Chore/tthp 3997 application performance improvements (#4436)
    * chore/TTHP-3997: Application improvements
    
    * chore/TTHP-3997: Application Improvements
    
    * simplify triggers
    
    * chore/TTHP-3997: fix breaking the loop to early
    
    * fix comments
    
    * fix migration for hasura
    
    * drop search fields and status if exists
    
    * Simplify GetOrganizationsProfiles
    
    * pause request for org overview tab
    
    * reaply migration for australia
    
    * fix migration
    
    * show spinner for individuals by level tab
    
    * more improvements
    
    * display org summary table faster
    
    * fix improvements
    
    * get enrollments only if certificate in defined statuses
    
    * display multiple enrollments, not just one
    
    * fix upcoming enrollments not showing for specific organization
    
    * change timestamp to reapply migration
    
    * Change upcoming enrollments again

---
+++ hasura/migrations/default/1723617806110_get_distinct_upcoming_enrollments/up.sql
@@ -0,0 +1,50 @@
+CREATE OR REPLACE VIEW "public"."upcoming_enrollments" AS 
+   SELECT DISTINCT ON (profile.id, course.id, org.id, cc.status)
+    profile.id AS profile_id,
+    course.course_level,
+    schedule.start AS schedule_start,
+    course.id AS course_id,
+    org.id AS org_id,
+    org.name AS org_name,
+    cc.status AS certification_status
+   FROM profile AS profile
+     JOIN course_participant AS cp ON cp.profile_id = profile.id
+     JOIN course AS course ON course.id = cp.course_id
+     JOIN organization AS org ON course.organization_id = org.id
+     JOIN course_schedule AS schedule ON schedule.course_id = course.id
+     JOIN course_certificate AS cc ON cc.profile_id = profile.id
+     JOIN course_participant AS course_p ON course_p.certificate_id = cc.id
+  WHERE schedule."end" > now() AND cc.status IN ('ACTIVE','ON_HOLD','EXPIRED_RECENTLY','EXPIRING_SOON')
+   AND profile.archived IS FALSE
+   AND (course_p.completed_evaluation IS TRUE OR cc.legacy_course_code IS NOT NULL);
+   
+CREATE OR REPLACE FUNCTION update_organizations_statistics(org_id uuid)
+    RETURNS VOID
+    LANGUAGE plpgsql
+AS $function$
+BEGIN
+    DELETE FROM organizations_statistics WHERE organization_id = org_id;
+    INSERT INTO organizations_statistics(organization_id, individuals, active_certifications, on_hold_certifications, expiring_soon_certifications, expired_recently_certifications)
+        VALUES (org_id, (SELECT COUNT(*) FROM organization_member WHERE organization_id = org_id),
+                (SELECT COUNT(DISTINCT cc.id) FROM course_certificate AS cc
+                    LEFT JOIN course_participant AS cp ON cc.id = cp.certificate_id
+                    LEFT JOIN profile AS p ON cc.profile_id = p.id
+                    LEFT JOIN organization_member as om ON om.profile_id = p.id
+                    WHERE om.organization_id = org_id AND cc.status = 'ACTIVE' AND p.archived IS FALSE AND (cp.completed_evaluation IS TRUE OR cc.legacy_course_code IS NOT NULL)),
+                (SELECT COUNT(DISTINCT cc.id) FROM course_certificate AS cc
+                    LEFT JOIN course_participant AS cp ON cc.id = cp.certificate_id
+                    LEFT JOIN profile AS p ON cc.profile_id = p.id
+                    LEFT JOIN organization_member as om ON om.profile_id = p.id
+                    WHERE om.organization_id = org_id AND cc.status = 'ON_HOLD' AND p.archived IS FALSE AND (cp.completed_evaluation IS TRUE OR cc.legacy_course_code IS NOT NULL)),
+                (SELECT COUNT(DISTINCT cc.id) FROM course_certificate AS cc
+                    LEFT JOIN course_participant AS cp ON cc.id = cp.certificate_id
+                    LEFT JOIN profile AS p ON cc.profile_id = p.id
+                    LEFT JOIN organization_member as om ON om.profile_id = p.id
+                    WHERE om.organization_id = org_id AND cc.status = 'EXPIRING_SOON' AND p.archived IS FALSE AND (cp.completed_evaluation IS TRUE OR cc.legacy_course_code IS NOT NULL)),
+                (SELECT COUNT(DISTINCT cc.id) FROM course_certificate AS cc
+                    LEFT JOIN course_participant AS cp ON cc.id = cp.certificate_id
+                    LEFT JOIN profile AS p ON cc.profile_id = p.id
+                    LEFT JOIN organization_member as om ON om.profile_id = p.id
+                    WHERE om.organization_id = org_id AND cc.status = 'EXPIRED_RECENTLY' AND p.archived IS FALSE AND (cp.completed_evaluation IS TRUE OR cc.legacy_course_code IS NOT NULL)));
+END;
+$function$;
