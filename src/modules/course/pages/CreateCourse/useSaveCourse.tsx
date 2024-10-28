40d5a1dde53f8c19115a805a98d5a9032321a38e
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2024-10-28 07:44:20 +0200

    Fix anz/tthp 4516 display missing price error (#4674)
    * fix-ANZ/TTHP-4510: Don't allow course creation without scheduled price in Australia, for ANZ
    
    * Display missing price error only on open/closed courses

--- src/modules/course/pages/CreateCourse/useSaveCourse.tsx
+++ src/modules/course/pages/CreateCourse/useSaveCourse.tsx
@@ -182,7 +182,9 @@ export function useSaveCourse(): {
   const isIndirectCourse = courseData?.type === Course_Type_Enum.Indirect
 
   const courseHasManualPrice = acl.isAustralia()
-    ? !isAustraliaCountry(courseData?.residingCountry)
+    ? !isAustraliaCountry(courseData?.residingCountry) &&
+      isOpenCourse &&
+      isClosedCourse
     : courseWithManualPrice({
         accreditedBy: courseData?.accreditedBy as Accreditors_Enum,
         courseType: courseData?.type as Course_Type_Enum,
