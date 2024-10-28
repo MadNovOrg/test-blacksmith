0d385ef41e615c5229d01ac2264833b8f2b8f094
iulian-lupastean <101259673+iulian-lupastean@users.noreply.github.com>
2024-10-28 11:58:40 +0200

    fix-ANZ/TTHP-4516: Don't show pricing error on indirect course (#4688)

--- src/modules/course/pages/CreateCourse/useSaveCourse.tsx
+++ src/modules/course/pages/CreateCourse/useSaveCourse.tsx
@@ -182,9 +182,7 @@ export function useSaveCourse(): {
   const isIndirectCourse = courseData?.type === Course_Type_Enum.Indirect
 
   const courseHasManualPrice = acl.isAustralia()
-    ? !isAustraliaCountry(courseData?.residingCountry) &&
-      isOpenCourse &&
-      isClosedCourse
+    ? !isAustraliaCountry(courseData?.residingCountry) && !isIndirectCourse
     : courseWithManualPrice({
         accreditedBy: courseData?.accreditedBy as Accreditors_Enum,
         courseType: courseData?.type as Course_Type_Enum,
