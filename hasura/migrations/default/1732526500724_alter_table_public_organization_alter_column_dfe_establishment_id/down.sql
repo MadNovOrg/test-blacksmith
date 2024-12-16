f10bd243c6d6e75dab9da01ba083904f9fcffd38
madalin-novac <135691767+madalin-novac@users.noreply.github.com>
2024-12-16 14:49:40 +0200

    fix: migrations (#4893)
    Co-authored-by: Marcel Novac <marcel.novac@Madalins-MacBook-Pro.local>

--- hasura/migrations/default/1732526500724_alter_table_public_organization_alter_column_dfe_establishment_id/down.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."organization" drop constraint "organization_dfe_establishment_id_key";
--- hasura/migrations/default/1732526500724_alter_table_public_organization_alter_column_dfe_establishment_id/up.sql
+++
@@ -1,1 +0,0 @@
-alter table "public"."organization" add constraint "organization_dfe_establishment_id_key" unique ("dfe_establishment_id");
