bea8423e35ce408c001ae1f93bd18d520888fde9
madalin-novac <135691767+madalin-novac@users.noreply.github.com>
2025-05-20 14:57:29 +0300

    fix (TTHP-5091): update course search fields on trainer delete (#5679)
    * fix (TTHP-5091): update course search fields on trainer delete
    
    * down.sql reverse sentences

---
+++ .changeset/spicy-ads-sit.md
@@ -0,0 +1,6 @@
+---
+'@teamteach/hub': patch
+---
+
+Update search_fields column of the course table to be take into consideration course_trainer deletion  
+
---
+++ hasura/migrations/default/1744792361094_run_sql_migration/down.sql
@@ -0,0 +1,2 @@
+DROP TRIGGER IF EXISTS update_course_search_on_trainer_delete ON course_trainer;
+DROP FUNCTION IF EXISTS update_course_search_fields_trigger();
