826a8cf2afae90ea317bfcaefbb0f1df54bc49df
madalin-novac <135691767+madalin-novac@users.noreply.github.com>
2025-08-07 15:37:27 +0300

    chore: test gtm analytics parameters capture (#6390)

--- src/lib/tag-manager.ts
+++ src/lib/tag-manager.ts
@@ -2,8 +2,11 @@ import { useEffect } from 'react'
 import TagManager from 'react-gtm-module'
 import { useLocation } from 'react-router-dom'
 
+import { useAuth } from '@app/context/auth'
+
 export const GTMPageTracker = () => {
   const location = useLocation()
+  const { profile } = useAuth()
 
   useEffect(() => {
     const pagePath =
@@ -12,9 +15,10 @@ export const GTMPageTracker = () => {
       dataLayer: {
         event: 'pageview',
         page: pagePath,
+        userId: profile?.id,
       },
     })
-  }, [location])
+  }, [location, profile?.id])
 
   return null
 }
